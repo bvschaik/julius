435a5ae1d345d1b48081f6861346b945269a6d4f
Bianca van Schaik <bvschaik@gmail.com>
2016-05-18 15:01:05 +0000

    Fix #101, cleanup

---
+++ src/UI/Logo.cpp
@@ -0,0 +1,32 @@
+#include "AllWindows.h"
+
+#include "../Graphics.h"
+#include "../Sound.h"
+#include "../Widget.h"
+
+#include "../Data/Constants.h"
+#include "../Data/Graphics.h"
+#include "../Data/Screen.h"
+#include "../Data/Mouse.h"
+#include "../Data/Types.h"
+
+void UI_Logo_init()
+{
+	Sound_Music_playIntro();
+}
+
+void UI_Logo_drawBackground()
+{
+	Graphics_clearScreen();
+	Graphics_drawImage(GraphicId(ID_Graphic_Logo), Data_Screen.offset640x480.x, Data_Screen.offset640x480.y);
+	Widget_GameText_drawCenteredColored(13, 7,
+		(Data_Screen.width - 320) / 2, Data_Screen.offset640x480.y + 462,
+		320, Font_NormalPlain, Color_White);
+}
+
+void UI_Logo_handleMouse()
+{
+	if (Data_Mouse.left.wentUp || Data_Mouse.right.wentUp) {
+		UI_Window_goTo(Window_MainMenu);
+	}
+}
