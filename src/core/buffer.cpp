1b6bec5338787e9fe517fe699ab268741cd0def7
dalerank <dalerankn8@gmail.com>
2017-11-25 13:40:40 +0300

    [bv] Remove macros from buffer

--- src/core/buffer.cpp
+++ src/core/buffer.cpp
@@ -2,9 +2,6 @@
 
 #include <string.h>
 
-#define CHECK_WRITE(b, s) if ((b->index + s) > b->size) { b->overflow = 1; return; }
-#define CHECK_READ(b, s) if ((b->index + s) > b->size) { b->overflow = 1; return 0; }
-
 void buffer_init(buffer *buffer, void *data, int size)
 {
     buffer->data = data;
@@ -24,103 +21,163 @@ void buffer_set(buffer *buffer, int offset)
     buffer->index = offset;
 }
 
+static int check_size(buffer *buffer, int size)
+{
+    if (buffer->index + size > buffer->size)
+    {
+        buffer->overflow = 1;
+        return 0;
+    }
+    return 1;
+}
+
 void buffer_write_u8(buffer *buffer, uint8_t value)
 {
-    CHECK_WRITE(buffer, 1);
-    buffer->data[buffer->index++] = value;
+    if (check_size(buffer, 1))
+    {
+        buffer->data[buffer->index++] = value;
+    }
 }
 
 void buffer_write_u16(buffer *buffer, uint16_t value)
 {
-    CHECK_WRITE(buffer, 2);
-    buffer->data[buffer->index++] = value & 0xff;
-    buffer->data[buffer->index++] = (value >> 8) & 0xff;
+    if (check_size(buffer, 2))
+    {
+        buffer->data[buffer->index++] = value & 0xff;
+        buffer->data[buffer->index++] = (value >> 8) & 0xff;
+    }
 }
 
 void buffer_write_u32(buffer *buffer, uint32_t value)
 {
-    CHECK_WRITE(buffer, 4);
-    buffer->data[buffer->index++] = value & 0xff;
-    buffer->data[buffer->index++] = (value >> 8) & 0xff;
-    buffer->data[buffer->index++] = (value >> 16) & 0xff;
-    buffer->data[buffer->index++] = (value >> 24) & 0xff;
+    if (check_size(buffer, 4))
+    {
+        buffer->data[buffer->index++] = value & 0xff;
+        buffer->data[buffer->index++] = (value >> 8) & 0xff;
+        buffer->data[buffer->index++] = (value >> 16) & 0xff;
+        buffer->data[buffer->index++] = (value >> 24) & 0xff;
+    }
 }
 
 void buffer_write_i8(buffer *buffer, int8_t value)
 {
-    CHECK_WRITE(buffer, 1);
-    buffer->data[buffer->index++] = value & 0xff;
+    if (check_size(buffer, 1))
+    {
+        buffer->data[buffer->index++] = value & 0xff;
+    }
 }
 
 void buffer_write_i16(buffer *buffer, int16_t value)
 {
-    CHECK_WRITE(buffer, 2);
-    buffer->data[buffer->index++] = value & 0xff;
-    buffer->data[buffer->index++] = (value >> 8) & 0xff;
+    if (check_size(buffer, 2))
+    {
+        buffer->data[buffer->index++] = value & 0xff;
+        buffer->data[buffer->index++] = (value >> 8) & 0xff;
+    }
 }
 
 void buffer_write_i32(buffer *buffer, int32_t value)
 {
-    CHECK_WRITE(buffer, 4);
-    buffer->data[buffer->index++] = value & 0xff;
-    buffer->data[buffer->index++] = (value >> 8) & 0xff;
-    buffer->data[buffer->index++] = (value >> 16) & 0xff;
-    buffer->data[buffer->index++] = (value >> 24) & 0xff;
+    if (check_size(buffer, 4))
+    {
+        buffer->data[buffer->index++] = value & 0xff;
+        buffer->data[buffer->index++] = (value >> 8) & 0xff;
+        buffer->data[buffer->index++] = (value >> 16) & 0xff;
+        buffer->data[buffer->index++] = (value >> 24) & 0xff;
+    }
 }
 
 void buffer_write_raw(buffer *buffer, const void *value, int size)
 {
-    CHECK_WRITE(buffer, size);
-    memcpy(&buffer->data[buffer->index], value, size);
-    buffer->index += size;
+    if (check_size(buffer, size))
+    {
+        memcpy(&buffer->data[buffer->index], value, size);
+        buffer->index += size;
+    }
 }
 
 uint8_t buffer_read_u8(buffer *buffer)
 {
-    CHECK_READ(buffer, 1);
-    return buffer->data[buffer->index++];
+    if (check_size(buffer, 1))
+    {
+        return buffer->data[buffer->index++];
+    }
+    else
+    {
+        return 0;
+    }
 }
 
 uint16_t buffer_read_u16(buffer *buffer)
 {
-    CHECK_READ(buffer, 2);
-    uint8_t b0 = buffer->data[buffer->index++];
-    uint8_t b1 = buffer->data[buffer->index++];
-    return (uint16_t) (b0 | (b1 << 8));
+    if (check_size(buffer, 2))
+    {
+        uint8_t b0 = buffer->data[buffer->index++];
+        uint8_t b1 = buffer->data[buffer->index++];
+        return (uint16_t) (b0 | (b1 << 8));
+    }
+    else
+    {
+        return 0;
+    }
 }
 
 uint32_t buffer_read_u32(buffer *buffer)
 {
-    CHECK_READ(buffer, 4);
-    uint8_t b0 = buffer->data[buffer->index++];
-    uint8_t b1 = buffer->data[buffer->index++];
-    uint8_t b2 = buffer->data[buffer->index++];
-    uint8_t b3 = buffer->data[buffer->index++];
-    return (uint32_t) (b0 | (b1 << 8) | (b2 << 16) | (b3 << 24));
+    if (check_size(buffer, 4))
+    {
+        uint8_t b0 = buffer->data[buffer->index++];
+        uint8_t b1 = buffer->data[buffer->index++];
+        uint8_t b2 = buffer->data[buffer->index++];
+        uint8_t b3 = buffer->data[buffer->index++];
+        return (uint32_t) (b0 | (b1 << 8) | (b2 << 16) | (b3 << 24));
+    }
+    else
+    {
+        return 0;
+    }
 }
 
 int8_t buffer_read_i8(buffer *buffer)
 {
-    CHECK_READ(buffer, 1);
-    return (int8_t) buffer->data[buffer->index++];
+    if (check_size(buffer, 1))
+    {
+        return (int8_t) buffer->data[buffer->index++];
+    }
+    else
+    {
+        return 0;
+    }
 }
 
 int16_t buffer_read_i16(buffer *buffer)
 {
-    CHECK_READ(buffer, 2);
-    uint8_t b0 = buffer->data[buffer->index++];
-    uint8_t b1 = buffer->data[buffer->index++];
-    return (int16_t) (b0 | (b1 << 8));
+    if (check_size(buffer, 2))
+    {
+        uint8_t b0 = buffer->data[buffer->index++];
+        uint8_t b1 = buffer->data[buffer->index++];
+        return (int16_t) (b0 | (b1 << 8));
+    }
+    else
+    {
+        return 0;
+    }
 }
 
 int32_t buffer_read_i32(buffer *buffer)
 {
-    CHECK_READ(buffer, 4);
-    uint8_t b0 = buffer->data[buffer->index++];
-    uint8_t b1 = buffer->data[buffer->index++];
-    uint8_t b2 = buffer->data[buffer->index++];
-    uint8_t b3 = buffer->data[buffer->index++];
-    return (int32_t) (b0 | (b1 << 8) | (b2 << 16) | (b3 << 24));
+    if (check_size(buffer, 4))
+    {
+        uint8_t b0 = buffer->data[buffer->index++];
+        uint8_t b1 = buffer->data[buffer->index++];
+        uint8_t b2 = buffer->data[buffer->index++];
+        uint8_t b3 = buffer->data[buffer->index++];
+        return (int32_t) (b0 | (b1 << 8) | (b2 << 16) | (b3 << 24));
+    }
+    else
+    {
+        return 0;
+    }
 }
 
 int buffer_read_raw(buffer *buffer, void *value, int max_size)
