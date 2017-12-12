28b2cac35e080c4c71390dc3aef91950a5589a88
Bianca van Schaik <bvschaik@gmail.com>
2017-12-12 22:51:05 +0100

    Internalize terrain grid

--- src/Data/Grid.h
+++ src/Data/Grid.h
@@ -5,13 +5,10 @@
 #include <stdint.h>
 
 #define UInt8_Grid(x) uint8_t x[GRID_SIZE * GRID_SIZE]
-#define UInt16_Grid(x) uint16_t x[GRID_SIZE * GRID_SIZE]
 
 enum {
 	GRID_SIZE = 162,
 };
-
-EXTERN UInt16_Grid(Data_Grid_terrain);
 
 EXTERN UInt8_Grid(Data_Grid_elevation);
 EXTERN UInt8_Grid(Data_Grid_aqueducts);
--- src/map/terrain.c
+++ src/map/terrain.c
@@ -2,59 +2,60 @@
 
 #include "map/grid.h"
 
+static grid_u16 terrain_grid;
 static grid_u16 terrain_grid_backup;
 
 int map_terrain_is(int grid_offset, int terrain)
 {
-    return Data_Grid_terrain[grid_offset] & terrain;
+    return terrain_grid.items[grid_offset] & terrain;
 }
 
 int map_terrain_get(int grid_offset)
 {
-    return Data_Grid_terrain[grid_offset];
+    return terrain_grid.items[grid_offset];
 }
 
 void map_terrain_set(int grid_offset, int terrain)
 {
-    Data_Grid_terrain[grid_offset] = terrain;
+    terrain_grid.items[grid_offset] = terrain;
 }
 
 void map_terrain_add(int grid_offset, int terrain)
 {
-    Data_Grid_terrain[grid_offset] |= terrain;
+    terrain_grid.items[grid_offset] |= terrain;
 }
 
 void map_terrain_remove(int grid_offset, int terrain)
 {
-    Data_Grid_terrain[grid_offset] &= ~terrain;
+    terrain_grid.items[grid_offset] &= ~terrain;
 }
 
 void map_terrain_remove_all(int terrain)
 {
-    map_grid_and_u16(Data_Grid_terrain, ~terrain);
+    map_grid_and_u16(terrain_grid.items, ~terrain);
 }
 
 void map_terrain_backup()
 {
-    map_grid_copy_u16(Data_Grid_terrain, terrain_grid_backup.items);
+    map_grid_copy_u16(terrain_grid.items, terrain_grid_backup.items);
 }
 
 void map_terrain_restore()
 {
-    map_grid_copy_u16(terrain_grid_backup.items, Data_Grid_terrain);
+    map_grid_copy_u16(terrain_grid_backup.items, terrain_grid.items);
 }
 
 void map_terrain_clear()
 {
-    map_grid_clear_u16(Data_Grid_terrain);
+    map_grid_clear_u16(terrain_grid.items);
 }
 
 void map_terrain_save_state(buffer *buf)
 {
-    map_grid_save_state_u16(Data_Grid_terrain, buf);
+    map_grid_save_state_u16(terrain_grid.items, buf);
 }
 
 void map_terrain_load_state(buffer *buf)
 {
-    map_grid_load_state_u16(Data_Grid_terrain, buf);
+    map_grid_load_state_u16(terrain_grid.items, buf);
 }
