2d092731d630d6c8a61501b5509eac9ab4088cae
Bianca van Schaik <bvschaik@gmail.com>
2017-12-16 19:37:23 +0100

    Use struct instead of building array

--- src/CityInfo_Tick.c
+++ src/CityInfo_Tick.c
@@ -18,11 +18,12 @@ void CityInfo_Tick_countBuildingTypes()
 	Data_CityInfo.numHospitalWorkers = 0;
 
 	for (int i = 1; i < MAX_BUILDINGS; i++) {
-		if (!BuildingIsInUse(i) || Data_Buildings[i].houseSize) {
+        struct Data_Building *b = &Data_Buildings[i];
+		if (!BuildingIsInUse(i) || b->houseSize) {
 			continue;
 		}
 		int isEntertainmentVenue = 0;
-        int type = Data_Buildings[i].type;
+        int type = b->type;
 		switch (type) {
             // SPECIAL TREATMENT
             // entertainment venues
@@ -31,23 +32,23 @@ void CityInfo_Tick_countBuildingTypes()
             case BUILDING_COLOSSEUM:
             case BUILDING_HIPPODROME:
                 isEntertainmentVenue = 1;
-                building_count_increase(type, Data_Buildings[i].numWorkers > 0);
+                building_count_increase(type, b->numWorkers > 0);
                 break;
 
             case BUILDING_BARRACKS:
                 Data_CityInfo.buildingBarracksBuildingId = i;
-                building_count_increase(type, Data_Buildings[i].numWorkers > 0);
+                building_count_increase(type, b->numWorkers > 0);
                 break;
 
             case BUILDING_HOSPITAL:
-                building_count_increase(type, Data_Buildings[i].numWorkers > 0);
-                Data_CityInfo.numHospitalWorkers += Data_Buildings[i].numWorkers;
+                building_count_increase(type, b->numWorkers > 0);
+                Data_CityInfo.numHospitalWorkers += b->numWorkers;
                 break;
             
             // water
             case BUILDING_RESERVOIR:
             case BUILDING_FOUNTAIN:
-                building_count_increase(type, Data_Buildings[i].hasWaterAccess);
+                building_count_increase(type, b->hasWaterAccess);
                 break;
 
             // DEFAULT TREATMENT
@@ -85,67 +86,67 @@ void CityInfo_Tick_countBuildingTypes()
 			case BUILDING_LARGE_TEMPLE_MARS:
 			case BUILDING_LARGE_TEMPLE_VENUS:
 			case BUILDING_ORACLE:
-				building_count_increase(type, Data_Buildings[i].numWorkers > 0);
+				building_count_increase(type, b->numWorkers > 0);
 				break;
 
 			// industry
 			case BUILDING_WHEAT_FARM:
-				building_count_industry_increase(RESOURCE_WHEAT, Data_Buildings[i].numWorkers > 0);
+				building_count_industry_increase(RESOURCE_WHEAT, b->numWorkers > 0);
 				break;
 			case BUILDING_VEGETABLE_FARM:
-				building_count_industry_increase(RESOURCE_VEGETABLES, Data_Buildings[i].numWorkers > 0);
+				building_count_industry_increase(RESOURCE_VEGETABLES, b->numWorkers > 0);
 				break;
 			case BUILDING_FRUIT_FARM:
-				building_count_industry_increase(RESOURCE_FRUIT, Data_Buildings[i].numWorkers > 0);
+				building_count_industry_increase(RESOURCE_FRUIT, b->numWorkers > 0);
 				break;
 			case BUILDING_OLIVE_FARM:
-				building_count_industry_increase(RESOURCE_OLIVES, Data_Buildings[i].numWorkers > 0);
+				building_count_industry_increase(RESOURCE_OLIVES, b->numWorkers > 0);
 				break;
 			case BUILDING_VINES_FARM:
-				building_count_industry_increase(RESOURCE_VINES, Data_Buildings[i].numWorkers > 0);
+				building_count_industry_increase(RESOURCE_VINES, b->numWorkers > 0);
 				break;
 			case BUILDING_PIG_FARM:
-				building_count_industry_increase(RESOURCE_MEAT, Data_Buildings[i].numWorkers > 0);
+				building_count_industry_increase(RESOURCE_MEAT, b->numWorkers > 0);
 				break;
 			case BUILDING_MARBLE_QUARRY:
-				building_count_industry_increase(RESOURCE_MARBLE, Data_Buildings[i].numWorkers > 0);
+				building_count_industry_increase(RESOURCE_MARBLE, b->numWorkers > 0);
 				break;
 			case BUILDING_IRON_MINE:
-				building_count_industry_increase(RESOURCE_IRON, Data_Buildings[i].numWorkers > 0);
+				building_count_industry_increase(RESOURCE_IRON, b->numWorkers > 0);
 				break;
 			case BUILDING_TIMBER_YARD:
-				building_count_industry_increase(RESOURCE_TIMBER, Data_Buildings[i].numWorkers > 0);
+				building_count_industry_increase(RESOURCE_TIMBER, b->numWorkers > 0);
 				break;
 			case BUILDING_CLAY_PIT:
-				building_count_industry_increase(RESOURCE_CLAY, Data_Buildings[i].numWorkers > 0);
+				building_count_industry_increase(RESOURCE_CLAY, b->numWorkers > 0);
 				break;
 			case BUILDING_WINE_WORKSHOP:
-				building_count_industry_increase(RESOURCE_WINE, Data_Buildings[i].numWorkers > 0);
+				building_count_industry_increase(RESOURCE_WINE, b->numWorkers > 0);
 				break;
 			case BUILDING_OIL_WORKSHOP:
-				building_count_industry_increase(RESOURCE_OIL, Data_Buildings[i].numWorkers > 0);
+				building_count_industry_increase(RESOURCE_OIL, b->numWorkers > 0);
 				break;
 			case BUILDING_WEAPONS_WORKSHOP:
-				building_count_industry_increase(RESOURCE_WEAPONS, Data_Buildings[i].numWorkers > 0);
+				building_count_industry_increase(RESOURCE_WEAPONS, b->numWorkers > 0);
 				break;
 			case BUILDING_FURNITURE_WORKSHOP:
-				building_count_industry_increase(RESOURCE_FURNITURE, Data_Buildings[i].numWorkers > 0);
+				building_count_industry_increase(RESOURCE_FURNITURE, b->numWorkers > 0);
 				break;
 			case BUILDING_POTTERY_WORKSHOP:
-				building_count_industry_increase(RESOURCE_POTTERY, Data_Buildings[i].numWorkers > 0);
+				building_count_industry_increase(RESOURCE_POTTERY, b->numWorkers > 0);
 				break;
 
 			// water-side
 			case BUILDING_WHARF:
-				if (Data_Buildings[i].numWorkers > 0) {
+				if (b->numWorkers > 0) {
 					++Data_CityInfo.numWorkingWharfs;
-					if (!Data_Buildings[i].data.other.boatFigureId) {
+					if (!b->data.other.boatFigureId) {
 						++Data_CityInfo.shipyardBoatsRequested;
 					}
 				}
 				break;
 			case BUILDING_DOCK:
-				if (Data_Buildings[i].numWorkers > 0 && Data_Buildings[i].hasWaterAccess) {
+				if (b->numWorkers > 0 && b->hasWaterAccess) {
 					if (Data_CityInfo.numWorkingDocks < 10) {
 						Data_CityInfo.workingDockBuildingIds[Data_CityInfo.numWorkingDocks] = i;
 					}
@@ -153,24 +154,24 @@ void CityInfo_Tick_countBuildingTypes()
 				}
 				break;
 		}
-		if (Data_Buildings[i].immigrantFigureId) {
-			figure *f = figure_get(Data_Buildings[i].immigrantFigureId);
+		if (b->immigrantFigureId) {
+			figure *f = figure_get(b->immigrantFigureId);
 			if (f->state != FigureState_Alive || f->destinationBuildingId != i) {
-				Data_Buildings[i].immigrantFigureId = 0;
+				b->immigrantFigureId = 0;
 			}
 		}
 		if (isEntertainmentVenue) {
 			// update number of shows
 			int shows = 0;
-			if (Data_Buildings[i].data.entertainment.days1 > 0) {
-				--Data_Buildings[i].data.entertainment.days1;
+			if (b->data.entertainment.days1 > 0) {
+				--b->data.entertainment.days1;
 				++shows;
 			}
-			if (Data_Buildings[i].data.entertainment.days2 > 0) {
-				--Data_Buildings[i].data.entertainment.days2;
+			if (b->data.entertainment.days2 > 0) {
+				--b->data.entertainment.days2;
 				++shows;
 			}
-			Data_Buildings[i].data.entertainment.numShows = shows;
+			b->data.entertainment.numShows = shows;
 		}
 	}
 	building_count_limit_hippodrome();
@@ -195,35 +196,36 @@ void CityInfo_Tick_distributeTreasuryOverForumsAndSenates()
 	}
 
 	for (int i = 1; i < MAX_BUILDINGS; i++) {
-		if (!BuildingIsInUse(i) || Data_Buildings[i].houseSize) {
+        struct Data_Building *b = &Data_Buildings[i];
+		if (!BuildingIsInUse(i) || b->houseSize) {
 			continue;
 		}
-		Data_Buildings[i].taxIncomeOrStorage = 0;
-		if (Data_Buildings[i].numWorkers <= 0) {
+		b->taxIncomeOrStorage = 0;
+		if (b->numWorkers <= 0) {
 			continue;
 		}
-		switch (Data_Buildings[i].type) {
+		switch (b->type) {
 			// ordered based on importance: most important gets the remainder
 			case BUILDING_SENATE_UPGRADED:
-				Data_Buildings[i].taxIncomeOrStorage = 8 * amountPerUnit + remainder;
+				b->taxIncomeOrStorage = 8 * amountPerUnit + remainder;
 				remainder = 0;
 				break;
 			case BUILDING_SENATE:
 				if (remainder && !building_count_active(BUILDING_SENATE_UPGRADED)) {
-					Data_Buildings[i].taxIncomeOrStorage = 5 * amountPerUnit + remainder;
+					b->taxIncomeOrStorage = 5 * amountPerUnit + remainder;
 					remainder = 0;
 				} else {
-					Data_Buildings[i].taxIncomeOrStorage = 5 * amountPerUnit;
+					b->taxIncomeOrStorage = 5 * amountPerUnit;
 				}
 				break;
 			case BUILDING_FORUM_UPGRADED:
 				if (remainder && !(
 					building_count_active(BUILDING_SENATE_UPGRADED) ||
 					building_count_active(BUILDING_SENATE))) {
-					Data_Buildings[i].taxIncomeOrStorage = 2 * amountPerUnit + remainder;
+					b->taxIncomeOrStorage = 2 * amountPerUnit + remainder;
 					remainder = 0;
 				} else {
-					Data_Buildings[i].taxIncomeOrStorage = 2 * amountPerUnit;
+					b->taxIncomeOrStorage = 2 * amountPerUnit;
 				}
 				break;
 			case BUILDING_FORUM:
@@ -231,10 +233,10 @@ void CityInfo_Tick_distributeTreasuryOverForumsAndSenates()
 					building_count_active(BUILDING_SENATE_UPGRADED) ||
 					building_count_active(BUILDING_SENATE) ||
 					building_count_active(BUILDING_FORUM_UPGRADED))) {
-					Data_Buildings[i].taxIncomeOrStorage = amountPerUnit + remainder;
+					b->taxIncomeOrStorage = amountPerUnit + remainder;
 					remainder = 0;
 				} else {
-					Data_Buildings[i].taxIncomeOrStorage = amountPerUnit;
+					b->taxIncomeOrStorage = amountPerUnit;
 				}
 				break;
 		}
