9bbb902bfb9c6e27c674e4981f4ff86f938cdf31
Polaretto <polaretto.93@gmail.com>
2018-02-12 22:09:40 +0100

    Visual C++ Compile Fix (#21)

--- test/sav/run.c
+++ test/sav/run.c
@@ -3,20 +3,26 @@
 #include "game/game.h"
 #include "game/settings.h"
 
-#ifndef __MINGW32__
+#if defined(__GNUC__) && !defined(__MINGW32__)
 #include <execinfo.h>
+#endif
+
+#ifdef _MSC_VER
+#include <direct.h>
+#define chdir _chdir
+#define getcwd _getcwd
+#else
+#include <unistd.h>
 #endif
 
 #include <signal.h>
 #include <stdlib.h>
 #include <stdio.h>
-#include <unistd.h>
-
 #include "sav_compare.h"
 
 static void handler(int sig)
 {
-#ifndef __MINGW32__
+#if defined(__GNUC__) && !defined(__MINGW32__)
     void *array[100];
     size_t size;
 
@@ -26,8 +32,10 @@ static void handler(int sig)
     // print out all the frames to stderr
     fprintf(stderr, "Error: signal %d:\n", sig);
     backtrace_symbols_fd(array, size, STDERR_FILENO);
-    exit(1);
+#else
+    fprintf(stderr, "Oops, crashed with signal %d :(\n", sig);
 #endif
+    exit(1);
 }
 
 static void run_ticks(int ticks)
@@ -44,18 +52,18 @@ static int run_autopilot(const char *input_saved_game, const char *output_saved_
 {
     printf("Running autopilot: %s --> %s in %d ticks\n", input_saved_game, output_saved_game, ticks_to_run);
     signal(SIGSEGV, handler);
-    
+
     chdir("data/sav");
     if (!game_pre_init()) {
         printf("Unable to run Game_preInit\n");
         return 1;
     }
-    
+
     if (!game_init()) {
         printf("Unable to run Game_init\n");
         return 2;
     }
-    
+
     if (!game_file_load_saved_game(input_saved_game)) {
         char wd[500];
         getcwd(wd, 500);
@@ -66,7 +74,7 @@ static int run_autopilot(const char *input_saved_game, const char *output_saved_
     printf("Saving game to %s\n", output_saved_game);
     game_file_write_saved_game(output_saved_game);
     printf("Done\n");
-    
+
     game_exit();
 
     return 0;
@@ -84,7 +92,8 @@ int main(int argc, char **argv)
     int ticks = atoi(argv[4]);
     if (run_autopilot(input, output, ticks) == 0) {
         return compare_files(expected, output);
-    } else {
+    }
+    else {
         return 1;
     }
 }
