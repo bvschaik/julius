6aa27d23e48738ecd8f8f319b9fb792e96757c43
Bianca van Schaik <bvschaik@gmail.com>
2018-11-03 11:55:32 +0100

    Split cursor logic from main julius file

---
+++ src/platform/cursor.c
@@ -0,0 +1,44 @@
+#include "game/system.h"
+#include "input/cursor.h"
+
+#include "SDL.h"
+
+static SDL_Cursor *cursors[CURSOR_MAX];
+
+static SDL_Cursor *init_cursor(const cursor *c)
+{
+    Uint8 data[4*32];
+    Uint8 mask[4*32];
+    int b = -1;
+    for (int i = 0; i < 32 * 32; i++) {
+        if (i % 8 ) {
+            data[b] <<= 1;
+            mask[b] <<= 1;
+        } else {
+            b++;
+            data[b] = mask[b] = 0;
+        }
+        switch (c->data[i]) {
+            case 'X':
+                data[b] |= 0x01;
+                // fallthrough
+            case '.':
+                mask[b] |= 0x01;
+                break;
+        }
+    }
+    return SDL_CreateCursor(data, mask, 32, 32, c->hotspot_x, c->hotspot_y);
+}
+
+void system_init_cursors()
+{
+    for (int i = 0; i < CURSOR_MAX; i++) {
+        cursors[i] = init_cursor(input_cursor_data(i));
+    }
+    system_set_cursor(CURSOR_ARROW);
+}
+
+void system_set_cursor(int cursor_id)
+{
+    SDL_SetCursor(cursors[cursor_id]);
+}
--- src/platform/julius.c
+++ src/platform/julius.c
@@ -9,7 +9,6 @@
 #include "graphics/screen.h"
 #include "graphics/text.h" // debug for fps counter
 #include "graphics/window.h"
-#include "input/cursor.h"
 #include "input/mouse.h"
 #include "platform/keyboard_input.h"
 
@@ -38,7 +37,6 @@ static struct {
     SDL_Window *window;
     SDL_Renderer *renderer;
     SDL_Texture *texture;
-    SDL_Cursor *cursors[CURSOR_MAX];
 } SDL;
 
 enum {
@@ -98,44 +96,6 @@ void system_toggle_fullscreen()
         event.user.code = USER_EVENT_FULLSCREEN;
     }
     SDL_PushEvent(&event);
-}
-
-void system_set_cursor(int cursor_id)
-{
-    SDL_SetCursor(SDL.cursors[cursor_id]);
-}
-
-static SDL_Cursor *init_cursor(const cursor *c)
-{
-    Uint8 data[4*32];
-    Uint8 mask[4*32];
-    int b = -1;
-    for (int i = 0; i < 32 * 32; i++) {
-        if (i % 8 ) {
-            data[b] <<= 1;
-            mask[b] <<= 1;
-        } else {
-            b++;
-            data[b] = mask[b] = 0;
-        }
-        switch (c->data[i]) {
-            case 'X':
-                data[b] |= 0x01;
-                // fallthrough
-            case '.':
-                mask[b] |= 0x01;
-                break;
-        }
-    }
-    return SDL_CreateCursor(data, mask, 32, 32, c->hotspot_x, c->hotspot_y);
-}
-
-void system_init_cursors()
-{
-    for (int i = 0; i < CURSOR_MAX; i++) {
-        SDL.cursors[i] = init_cursor(input_cursor_data(i));
-    }
-    system_set_cursor(CURSOR_ARROW);
 }
 
 static Uint32 last;
