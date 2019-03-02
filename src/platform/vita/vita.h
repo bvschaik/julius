f8d0de64ea0bda7fecada1f4e1842744f3476a21
devnoname120 <devnoname120+gh@gmail.com>
2019-03-02 16:21:53 +0100

    vita: ifdef __vita__ guard on vita.h

--- src/platform/vita/vita.h
+++ src/platform/vita/vita.h
@@ -1,10 +1,13 @@
 #ifndef PLATFORM_VITA_H
 #define PLATFORM_VITA_H
 
+#ifdef __vita__
+
 #define VITA_PATH_PREFIX "ux0:/data/julius/"
 
 int chdir(const char *path);
 
 char* vita_prepend_path(const char *path);
 
+#endif // __vita__
 #endif // PLATFORM_VITA_H
