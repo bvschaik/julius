1a0bc68d6a845c02748f327a993ab82f9ba73be7
devnoname120 <devnoname120@gmail.com>
2019-03-22 21:30:33 +0100

    Bintray: Add Switch deployment (#132)

--- .ci_scripts/bintray_conf.sh
+++ .ci_scripts/bintray_conf.sh
@@ -30,4 +30,32 @@ cat > "bintray.json" <<EOF
   "publish": true
 }
 EOF
+elif [ $DEPLOY = "switch" ]
+then
+cat > "bintray.json" <<EOF
+{
+  "package": {
+    "subject": "bvschaik",
+    "repo": "julius",
+    "name": "switch-unstable",
+    "licenses": ["AGPL-V3"],
+    "vcs_url": "https://github.com/bvschaik/julius.git"
+  },
+
+  "version": {
+    "name": "$VERSION",
+    "released": "$(date +'%Y-%m-%d')",
+    "desc": "Automated Switch build for Travis-CI job: $TRAVIS_JOB_WEB_URL"
+  },
+
+  "files": [
+    {
+      "includePattern": "${build_dir}/julius_switch.zip",
+      "uploadPattern": "julius-$VERSION.zip"
+    }
+  ],
+
+  "publish": true
+}
+EOF
 fi
