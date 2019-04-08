1f4119984b49455d27f3cbec111d79510bd4b8f0
Bianca van Schaik <bvschaik@gmail.com>
2019-04-08 22:52:35 +0200

    Make speed settings dialog wider
    Issue #138

--- src/window/speed_options.c
+++ src/window/speed_options.c
@@ -1,6 +1,5 @@
 #include "speed_options.h"
 
-
 #include "game/settings.h"
 #include "graphics/arrow_button.h"
 #include "graphics/generic_button.h"
@@ -45,24 +44,24 @@ static void draw_foreground(void)
 {
     graphics_in_dialog();
 
-    outer_panel_draw(96, 80, 18, 14);
+    outer_panel_draw(80, 80, 20, 14);
     // ok/cancel labels
     label_draw(144, 232, 12, focus_button_id == 1 ? 1 : 2);
     label_draw(144, 262, 12, focus_button_id == 2 ? 1 : 2);
-    
+
     // title
-    lang_text_draw_centered(45, 0, 128, 92, 224, FONT_LARGE_BLACK);
+    lang_text_draw_centered(45, 0, 96, 92, 288, FONT_LARGE_BLACK);
     // ok/cancel label texts
     lang_text_draw_centered(45, 4, 128, 236, 224, FONT_NORMAL_GREEN);
     lang_text_draw_centered(45, 1, 128, 266, 224, FONT_NORMAL_GREEN);
     // game speed
-    lang_text_draw(45, 2, 144, 146, FONT_NORMAL_PLAIN);
-    text_draw_percentage(setting_game_speed(), 296, 146, FONT_NORMAL_PLAIN);
+    lang_text_draw(45, 2, 112, 146, FONT_NORMAL_PLAIN);
+    text_draw_percentage(setting_game_speed(), 328, 146, FONT_NORMAL_PLAIN);
     // scroll speed
-    lang_text_draw(45, 3, 144, 182, FONT_NORMAL_PLAIN);
-    text_draw_percentage(setting_scroll_speed(), 296, 182,FONT_NORMAL_PLAIN);
+    lang_text_draw(45, 3, 112, 182, FONT_NORMAL_PLAIN);
+    text_draw_percentage(setting_scroll_speed(), 328, 182, FONT_NORMAL_PLAIN);
 
-    arrow_buttons_draw(128, 40, arrow_buttons, 4);
+    arrow_buttons_draw(160, 40, arrow_buttons, 4);
     graphics_reset_dialog();
 }
 
@@ -74,7 +73,7 @@ static void handle_mouse(const mouse *m)
     } else {
         const mouse *m_dialog = mouse_in_dialog(m);
         if (!generic_buttons_handle_mouse(m_dialog, 0, 0, buttons, 2, &focus_button_id)) {
-            arrow_buttons_handle_mouse(m_dialog, 128, 40, arrow_buttons, 4);
+            arrow_buttons_handle_mouse(m_dialog, 160, 40, arrow_buttons, 4);
         }
     }
 }
