22cc280a9a5b3c677ea8adbcf1e7f7174101d0e7
Bianca van Schaik <bvschaik@gmail.com>
2019-04-14 10:58:02 +0200

    Run Release builds for Vita/Switch/Mac
    They should run slightly faster because of compiler optimizations

--- .ci_scripts/run_cmake.sh
+++ .ci_scripts/run_cmake.sh
@@ -2,11 +2,15 @@
 
 case "$BUILD_TARGET" in
 "vita")
-	docker exec vitasdk /bin/bash -c "mkdir build && cd build && cmake -DVITA_BUILD=ON .."
+	docker exec vitasdk /bin/bash -c "mkdir build && cd build && cmake -DCMAKE_BUILD_TYPE=Release -DVITA_BUILD=ON .."
 	;;
 "switch")
-	docker exec switchdev /bin/bash -c "mkdir build && cd build && cmake -DSWITCH_BUILD=ON .."
+	docker exec switchdev /bin/bash -c "mkdir build && cd build && cmake -DCMAKE_BUILD_TYPE=Release -DSWITCH_BUILD=ON .."
+	;;
+"mac")
+	mkdir build && cd build && cmake -DCMAKE_BUILD_TYPE=Release ..
 	;;
 *)
 	mkdir build && cd build && cmake ..
+	;;
 esac
