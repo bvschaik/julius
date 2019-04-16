a5babe97529db9dd79020b4dcd25026ebd621949
devnoname120 <devnoname120@gmail.com>
2019-04-16 07:30:45 +0200

    Bintray link: Point to file tab directly (#141)

--- .ci_scripts/bintray_link.sh
+++ .ci_scripts/bintray_link.sh
@@ -3,4 +3,4 @@
 RELEASE=$(grep '"name":' bintray.json | head -n1 | grep -o -E '[^":, ]+' | tail -n1)
 VERSION=$(cat res/version.txt)
 
-echo "Bintray link: https://bintray.com/bvschaik/julius/$RELEASE/$VERSION"
+echo "Bintray link: https://bintray.com/bvschaik/julius/$RELEASE/$VERSION#files"
