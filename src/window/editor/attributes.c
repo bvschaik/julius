1d38d29bce823fac0ab85c623b1a56059a3be751
Bianca van Schaik <bvschaik@gmail.com>
2019-04-19 20:13:39 +0200

    Add button hover effects, fix images

--- src/window/editor/attributes.c
+++ src/window/editor/attributes.c
@@ -1,8 +1,9 @@
 #include "attributes.h"
 
-#include "core/image_group.h"
+#include "core/image_group_editor.h"
 #include "game/resource.h"
 #include "graphics/button.h"
+#include "graphics/generic_button.h"
 #include "graphics/graphics.h"
 #include "graphics/image.h"
 #include "graphics/lang_text.h"
@@ -13,6 +14,21 @@
 #include "scenario/editor.h"
 #include "scenario/property.h"
 #include "window/editor/map.h"
+
+static void button_click(int, int);
+
+static generic_button buttons[] = {
+    {212, 76, 462, 106, GB_IMMEDIATE, button_click, button_none, 1, 0},
+    {212, 116, 462, 146, GB_IMMEDIATE, button_click, button_none, 2, 0},
+    {212, 156, 462, 186, GB_IMMEDIATE, button_click, button_none, 3, 0},
+    {212, 196, 462, 226, GB_IMMEDIATE, button_click, button_none, 4, 0},
+    {212, 236, 462, 266, GB_IMMEDIATE, button_click, button_none, 5, 0},
+    {212, 276, 462, 306, GB_IMMEDIATE, button_click, button_none, 6, 0},
+    {212, 316, 462, 346, GB_IMMEDIATE, button_click, button_none, 7, 0},
+    {212, 356, 462, 386, GB_IMMEDIATE, button_click, button_none, 8, 0},
+    {212, 396, 462, 426, GB_IMMEDIATE, button_click, button_none, 9, 0},
+    {212, 436, 462, 466, GB_IMMEDIATE, button_click, button_none, 10, 0},
+};
 
 static int focus_button_id;
 
@@ -45,7 +61,7 @@ static void draw_foreground(void)
         lang_text_draw_year(scenario_property_start_year() + request.year, 222, 165, FONT_NORMAL_BLACK);
         int width = text_draw_number(request.amount, '@', " ", 312, 165, FONT_NORMAL_BLACK);
         int offset = request.resource + resource_image_offset(request.resource, RESOURCE_IMAGE_ICON);
-        image_draw(image_group(GROUP_RESOURCE_ICONS) + offset, 322 + width, 160);
+        image_draw(image_group(GROUP_EDITOR_RESOURCE_ICONS) + offset, 322 + width, 160);
     } else {
         lang_text_draw_centered(44, 19, 212, 165, 250, FONT_NORMAL_BLACK);
     }
@@ -83,17 +99,22 @@ static void draw_foreground(void)
     lang_text_draw_centered(44, 94, 212, 445, 250, FONT_NORMAL_BLACK);
 
     button_border_draw(18, 278, 184, 144, 0);
-    image_draw(image_group(GROUP_SCENARIO_IMAGE) + scenario_image_id(), 20, 280);
+    image_draw(image_group(GROUP_EDITOR_SCENARIO_IMAGE) + scenario_image_id(), 20, 280);
 
     graphics_reset_dialog();
 }
-
 
 static void handle_mouse(const mouse *m)
 {
     if (m->right.went_down) {
         window_editor_map_show();
     }
+    const mouse *m_dialog = mouse_in_dialog(m);
+    generic_buttons_handle_mouse(m_dialog, 0, 0, buttons, 10, &focus_button_id);
+}
+
+static void button_click(int type, int param2)
+{
 }
 
 void window_editor_attributes_show(void)
