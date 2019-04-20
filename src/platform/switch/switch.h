1c26ca5f439107e587d214235ee170d912989644
rsn8887 <rsn8887@users.noreply.github.com>
2019-04-20 08:00:41 -0500

    Switch: add two additional touch modes, minus button toggles modes (#144)
    Press minus to toggle between touch modes
    1) touchpad mode (tap to click)
    2) jump-to-finger mode (no tap to click)
    3) original julius touch control mode

--- src/platform/switch/switch.h
+++ src/platform/switch/switch.h
@@ -6,4 +6,15 @@
 #define SWITCH_PIXEL_WIDTH 1920
 #define SWITCH_PIXEL_HEIGHT 1080
 
+enum {
+    TOUCH_MODE_TOUCHPAD             = 0, // drag the pointer and tap-click like on a touchpad (default)
+    TOUCH_MODE_DIRECT               = 1, // pointer jumps to finger but doesn't click on tap
+    TOUCH_MODE_ORIGINAL             = 2, // original julius touch mode as on any other system: pointer jumps to finger and clicks on tap
+    NUM_TOUCH_MODES                 = 3
+};
+
+extern int last_mouse_x; // defined in switch_input.c
+extern int last_mouse_y; // defined in switch_input.c
+extern int touch_mode; // defined in switch_input.c
+
 #endif // PLATFORM_SWITCH_H
