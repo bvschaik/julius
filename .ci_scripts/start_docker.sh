79c8557d99cac421b35561365f24f4b2cc1d6023
devnoname120 <devnoname120@gmail.com>
2019-06-26 14:37:44 +0200

    vitasdk docker: use tagged release

--- .ci_scripts/start_docker.sh
+++ .ci_scripts/start_docker.sh
@@ -2,7 +2,10 @@
 
 case "$BUILD_TARGET" in
 "vita")
-	docker run -d --name vitasdk --workdir /build/git -v "${PWD}:/build/git" gnuton/vitasdk-docker tail -f /dev/null
+	# Note: Using a tagged version of the container to make sure that it's not updated unexpectedly
+	# You can update the tag by obtaining a recent one from here: https://hub.docker.com/r/gnuton/vitasdk-docker/tags
+	# Make sure that it compiles correctly and runs on a Vita prior to pushing the change
+	docker run -d --name vitasdk --workdir /build/git -v "${PWD}:/build/git" gnuton/vitasdk-docker:20190626 tail -f /dev/null
 	;;
 "switch")
 	docker run -d --name switchdev --workdir /build/git -v "${PWD}:/build/git" rsn8887/switchdev tail -f /dev/null
