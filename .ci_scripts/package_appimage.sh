d5a793ab18a7523a75947947c3ff264910b66651
devnoname120 <devnoname120@gmail.com>
2019-10-23 00:23:45 +0200

    AppImage: Display libs available to ldconfig

--- .ci_scripts/package_appimage.sh
+++ .ci_scripts/package_appimage.sh
@@ -3,7 +3,7 @@
 cd build
 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib/
-ldconfig -p | grep SDL
+echo "Available libraries on this system ==>\n$(ldconfig -p)"
 
 wget https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage
 chmod +x linuxdeploy*.AppImage
