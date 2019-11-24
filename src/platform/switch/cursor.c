a652b3a6d7d09e76e417b8836e3477d57e6d1b5a
Bianca van Schaik <bvschaik@gmail.com>
2019-11-24 17:43:11 +0100

    Fix cursor on Vita and Switch builds

--- src/platform/switch/cursor.c
+++ src/platform/switch/cursor.c
@@ -16,27 +16,27 @@ extern struct {
 
 static SDL_Texture *init_cursor(const cursor *c)
 {
-    SDL_Texture *tex = SDL_CreateTexture(SDL.renderer, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STATIC, 32, 32);
+    SDL_Texture *tex = SDL_CreateTexture(SDL.renderer, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STATIC, c->width, c->height);
 
-    uint32_t pixels[32 * 32];
+    uint32_t pixels[c->width * c->height];
 
-    for (int i = 0; i < 32; i++) {
-        for (int j = 0; j < 32; j++) {
-            switch (c->data[i * 32 + j]) {
-                case 'X':
-                    pixels[i * 32 + j] =  0xFF000000; //black
+    for (int i = 0; i < c->height; i++) {
+        for (int j = 0; j < c->width; j++) {
+            switch (c->data[i * c->width + j]) {
+                case '#':
+                    pixels[i * c->width + j] =  0xFF000000; //black
                     break;
-                case '.':
-                    pixels[i * 32 + j] = 0xFFFFFFFF; //white
+                case '\'':
+                    pixels[i * c->width + j] = 0xFFFFFFFF; //white
                     break;
                 case ' ':
-                    pixels[i * 32 + j] = 0x00000000; //transparent
+                    pixels[i * c->width + j] = 0x00000000; //transparent
                     break; // Transparent is 0x00000000 which is the default value of the buffer
             }
         }
     }
 
-    SDL_UpdateTexture(tex, NULL, pixels, 32 * 4);
+    SDL_UpdateTexture(tex, NULL, pixels, c->width * 4);
     SDL_SetTextureBlendMode(tex, SDL_BLENDMODE_BLEND);
 
     return tex;
