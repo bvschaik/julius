31ba33f7e64ee1016c486e0a358a7450cfabb1da
crudelios <crudelios@gmail.com>
2019-11-26 22:16:31 +0000

    Make sure travis fails successfully (travis was reporting builds as successful even though sometimes compilation or tests failed).

--- .ci_scripts/run_build.sh
+++ .ci_scripts/run_build.sh
@@ -8,14 +8,14 @@ case "$BUILD_TARGET" in
 	docker exec switchdev /bin/bash -c "cd build && make"
 	;;
 "mac")
-	cd build && make && make test && make install
-	echo "Creating disk image"
+	cd build && make && make test && make install && \
+	echo "Creating disk image" && \
 	hdiutil create -volname Julius -srcfolder julius.app -ov -format UDZO julius.dmg
 	;;
 "appimage")
-	cd build && make && make test
-	make DESTDIR=AppDir install
-	cd ..
+	cd build && make && make test && \
+	make DESTDIR=AppDir install && \
+	cd .. && \
 	./.ci_scripts/package_appimage.sh
 	;;
 *)
