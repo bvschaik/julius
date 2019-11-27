e73e83ed34526d1462e1fbbf088a63324b7b648a
Bianca van Schaik <bvschaik@gmail.com>
2019-11-27 08:41:11 +0100

    Revert back to 32x32 cursors for vita and switch

--- src/platform/vita/cursor.c
+++ src/platform/vita/cursor.c
@@ -6,22 +6,25 @@
 
 #include "SDL.h"
 
+#define CURSOR_SIZE 32
+#define ALPHA_OPAQUE (0xFFu << 24)
+
 vita2d_texture *cursors[CURSOR_MAX];
 vita2d_texture *current_cursor;
 
 static vita2d_texture *init_cursor(const cursor *c)
 {
-    vita2d_texture *tex = vita2d_create_empty_texture_format(c->width, c->height, SCE_GXM_TEXTURE_FORMAT_U8U8U8U8_ARGB);
+    vita2d_texture *tex = vita2d_create_empty_texture_format(CURSOR_SIZE, CURSOR_SIZE, SCE_GXM_TEXTURE_FORMAT_U8U8U8U8_ARGB);
     color_t *cursor_buf = vita2d_texture_get_datap(tex);
 
-    for (int i = 0; i < c->height; i++) {
-        for (int j = 0; j < c->width; j++) {
-            switch (c->data[i * c->width + j]) {
+    for (int y = 0; y < c->height; y++) {
+        for (int x = 0; x < c->width; x++) {
+            switch (c->data[y * c->width + x]) {
                 case '#':
-                    cursor_buf[i * c->width + j] = COLOR_BLACK | 0xFFu << 24;
+                    cursor_buf[y * CURSOR_SIZE + x] = COLOR_BLACK | ALPHA_OPAQUE;
                     break;
                 case '\'':
-                    cursor_buf[i * c->width + j] = COLOR_WHITE | 0xFFu << 24;
+                    cursor_buf[y * CURSOR_SIZE + x] = COLOR_WHITE | ALPHA_OPAQUE;
                     break;
                 case ' ':
                     break; // Transparent is 0x00000000 which is the default value of the buffer
