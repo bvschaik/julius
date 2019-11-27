9012c24f9f7af276ce09e4da5ac995065306cbaa
Bianca van Schaik <bvschaik@gmail.com>
2019-11-27 08:41:11 +0100

    Revert back to 32x32 cursors for vita and switch

--- src/platform/switch/cursor.c
+++ src/platform/switch/cursor.c
@@ -5,6 +5,9 @@
 
 #include "SDL.h"
 
+#define CURSOR_SIZE 32
+#define ALPHA_OPAQUE (0xFFu << 24)
+
 static SDL_Texture *cursors[CURSOR_MAX];
 SDL_Texture *current_cursor;
 
@@ -16,27 +19,27 @@ extern struct {
 
 static SDL_Texture *init_cursor(const cursor *c)
 {
-    SDL_Texture *tex = SDL_CreateTexture(SDL.renderer, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STATIC, c->width, c->height);
+    SDL_Texture *tex = SDL_CreateTexture(SDL.renderer, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STATIC, CURSOR_SIZE, CURSOR_SIZE);
 
-    uint32_t pixels[c->width * c->height];
+    uint32_t pixels[CURSOR_SIZE * CURSOR_SIZE];
 
-    for (int i = 0; i < c->height; i++) {
-        for (int j = 0; j < c->width; j++) {
-            switch (c->data[i * c->width + j]) {
+    for (int y = 0; y < c->height; y++) {
+        for (int x = 0; x < c->width; x++) {
+            switch (c->data[y * c->width + x]) {
                 case '#':
-                    pixels[i * c->width + j] =  0xFF000000; //black
+                    pixels[y * CURSOR_SIZE + x] = COLOR_BLACK | ALPHA_OPAQUE;
                     break;
                 case '\'':
-                    pixels[i * c->width + j] = 0xFFFFFFFF; //white
+                    pixels[y * CURSOR_SIZE + x] = COLOR_WHITE | ALPHA_OPAQUE;
                     break;
                 case ' ':
-                    pixels[i * c->width + j] = 0x00000000; //transparent
-                    break; // Transparent is 0x00000000 which is the default value of the buffer
+                    pixels[y * CURSOR_SIZE + x] = 0x00000000; // transparent
+                    break;
             }
         }
     }
 
-    SDL_UpdateTexture(tex, NULL, pixels, c->width * 4);
+    SDL_UpdateTexture(tex, NULL, pixels, CURSOR_SIZE * 4);
     SDL_SetTextureBlendMode(tex, SDL_BLENDMODE_BLEND);
 
     return tex;
