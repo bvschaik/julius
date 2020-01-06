2a59cfd4109252c951abebb104aaf2856613fe6a
Bianca van Schaik <bvschaik@gmail.com>
2020-01-06 19:59:24 +0100

    Always show editor button on main menu
    When the player does not have the editor, they will get a popup
    Fixes #203

--- src/window/main_menu.c
+++ src/window/main_menu.c
@@ -12,20 +12,19 @@
 #include "window/cck_selection.h"
 #include "window/file_dialog.h"
 #include "window/new_career.h"
+#include "window/plain_message_dialog.h"
 #include "window/popup_dialog.h"
 
 static void button_click(int type, int param2);
 
-static int editor_button_enabled = 0;
-
 static int focus_button_id;
 
 static generic_button buttons[] = {
-    {192, 100, 448, 125, GB_IMMEDIATE, button_click, button_none, 1, 0},
-    {192, 140, 448, 165, GB_IMMEDIATE, button_click, button_none, 2, 0},
-    {192, 180, 448, 205, GB_IMMEDIATE, button_click, button_none, 3, 0},
-    {192, 220, 448, 245, GB_IMMEDIATE, button_click, button_none, 4, 0},
-    {192, 260, 448, 285, GB_IMMEDIATE, button_click, button_none, 5, 0},
+    {192, 100, 448, 125, GB_ON_MOUSE_UP, button_click, button_none, 1, 0},
+    {192, 140, 448, 165, GB_ON_MOUSE_UP, button_click, button_none, 2, 0},
+    {192, 180, 448, 205, GB_ON_MOUSE_UP, button_click, button_none, 3, 0},
+    {192, 220, 448, 245, GB_ON_MOUSE_UP, button_click, button_none, 4, 0},
+    {192, 260, 448, 285, GB_ON_MOUSE_UP, button_click, button_none, 5, 0},
 };
 
 static void draw_background(void)
@@ -44,27 +43,20 @@ static void draw_foreground(void)
     large_label_draw(192, 140, 16, focus_button_id == 2 ? 1 : 0);
     large_label_draw(192, 180, 16, focus_button_id == 3 ? 1 : 0);
     large_label_draw(192, 220, 16, focus_button_id == 4 ? 1 : 0);
-    if (editor_button_enabled) {
-        large_label_draw(192, 260, 16, focus_button_id == 5 ? 1 : 0);
-    }
+    large_label_draw(192, 260, 16, focus_button_id == 5 ? 1 : 0);
 
     lang_text_draw_centered(30, 1, 192, 106, 256, FONT_NORMAL_GREEN);
     lang_text_draw_centered(30, 2, 192, 146, 256, FONT_NORMAL_GREEN);
     lang_text_draw_centered(30, 3, 192, 186, 256, FONT_NORMAL_GREEN);
-    if (editor_button_enabled) {
-        lang_text_draw_centered(9, 8, 192, 226, 256, FONT_NORMAL_GREEN);
-        lang_text_draw_centered(30, 5, 192, 266, 256, FONT_NORMAL_GREEN);
-    } else {
-        lang_text_draw_centered(30, 5, 192, 226, 256, FONT_NORMAL_GREEN);
-    }
+    lang_text_draw_centered(9, 8, 192, 226, 256, FONT_NORMAL_GREEN);
+    lang_text_draw_centered(30, 5, 192, 266, 256, FONT_NORMAL_GREEN);
 
     graphics_reset_dialog();
 }
 
 static void handle_mouse(const mouse *m)
 {
-    int num_buttons = editor_button_enabled ? 5 : 4;
-    generic_buttons_handle_mouse(mouse_in_dialog(m), 0, 0, buttons, num_buttons, &focus_button_id);
+    generic_buttons_handle_mouse(mouse_in_dialog(m), 0, 0, buttons, 5, &focus_button_id);
 }
 
 static void confirm_exit(int accepted)
@@ -82,9 +74,16 @@ static void button_click(int type, int param2)
         window_file_dialog_show(FILE_TYPE_SAVED_GAME, FILE_DIALOG_LOAD);
     } else if (type == 3) {
         window_cck_selection_show();
-    } else if (editor_button_enabled && type == 4) {
-        game_init_editor();
-    } else if ((editor_button_enabled && type == 5) || (!editor_button_enabled && type == 4)) {
+    } else if (type == 4) {
+        if (!editor_is_present() || !game_init_editor()) {
+            window_plain_message_dialog_show(
+                "Editor not installed",
+                "Your Caesar 3 installation does not contain the editor files. "
+                "You can download them from:\n"
+                "https://bintray.com/bvschaik/caesar3-editor"
+            );
+        }
+    } else if (type == 5) {
         window_popup_dialog_show(POPUP_DIALOG_QUIT, confirm_exit, 1);
     }
 }
@@ -97,6 +96,5 @@ void window_main_menu_show(void)
         draw_foreground,
         handle_mouse
     };
-    editor_button_enabled = editor_is_present();
     window_show(&window);
 }
