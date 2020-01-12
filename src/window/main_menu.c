b1ab1cd0493c8a9201dcb38eedd0798d53874e38
Bianca van Schaik <bvschaik@gmail.com>
2020-01-12 16:44:27 +0100

    Add button to play intro movie to main menu
    Issue #213

--- src/window/main_menu.c
+++ src/window/main_menu.c
@@ -6,17 +6,20 @@
 #include "graphics/generic_button.h"
 #include "graphics/graphics.h"
 #include "graphics/image.h"
+#include "graphics/image_button.h"
 #include "graphics/lang_text.h"
 #include "graphics/panel.h"
 #include "graphics/window.h"
 #include "sound/music.h"
 #include "window/cck_selection.h"
 #include "window/file_dialog.h"
+#include "window/intro_video.h"
 #include "window/new_career.h"
 #include "window/plain_message_dialog.h"
 #include "window/popup_dialog.h"
 
 static void button_click(int type, int param2);
+static void button_intro_movie(int param1, int param2);
 
 static int focus_button_id;
 
@@ -27,6 +30,9 @@ static generic_button buttons[] = {
     {192, 220, 448, 245, GB_ON_MOUSE_UP, button_click, button_none, 4, 0},
     {192, 260, 448, 285, GB_ON_MOUSE_UP, button_click, button_none, 5, 0},
 };
+
+static image_button movie_button =
+    {591, 442, 33, 22, IB_NORMAL, 89, 0, button_intro_movie, button_none, 0, 0, 1};
 
 static void draw_background(void)
 {
@@ -52,12 +58,18 @@ static void draw_foreground(void)
     lang_text_draw_centered(9, 8, 192, 226, 256, FONT_NORMAL_GREEN);
     lang_text_draw_centered(30, 5, 192, 266, 256, FONT_NORMAL_GREEN);
 
+    image_buttons_draw(0, 0, &movie_button, 1);
+
     graphics_reset_dialog();
 }
 
 static void handle_mouse(const mouse *m)
 {
-    generic_buttons_handle_mouse(mouse_in_dialog(m), 0, 0, buttons, 5, &focus_button_id);
+    const mouse *m_dialog = mouse_in_dialog(m);
+    int dummy;
+    if (!image_buttons_handle_mouse(m_dialog, 0, 0, &movie_button, 1, &dummy)) {
+        generic_buttons_handle_mouse(m_dialog, 0, 0, buttons, 5, &focus_button_id);
+    }
 }
 
 static void confirm_exit(int accepted)
@@ -89,6 +101,11 @@ static void button_click(int type, int param2)
     } else if (type == 5) {
         window_popup_dialog_show(POPUP_DIALOG_QUIT, confirm_exit, 1);
     }
+}
+
+static void button_intro_movie(int param1, int param2)
+{
+    window_intro_video_show();
 }
 
 void window_main_menu_show(int restart_music)
