f6a47fa4904215b67ef9fad184172a66bf744571
Keriew <59965174+Keriew@users.noreply.github.com>
2020-01-17 14:17:27 +0100

    Grand Festival allows the blessing again.

--- src/city/festival.c
+++ src/city/festival.c
@@ -111,7 +111,10 @@ static void throw_party(void)
     switch (city_data.festival.planned.size) {
         case FESTIVAL_SMALL: city_message_post(1, MESSAGE_SMALL_FESTIVAL, 0, 0); break;
         case FESTIVAL_LARGE: city_message_post(1, MESSAGE_LARGE_FESTIVAL, 0, 0); break;
-        case FESTIVAL_GRAND: city_message_post(1, MESSAGE_GRAND_FESTIVAL, 0, 0); break;
+        case FESTIVAL_GRAND: 
+            city_message_post(1, MESSAGE_GRAND_FESTIVAL, 0, 0); 
+            city_data.religion.gods[city_data.festival.planned.god].blessing_done = 0;
+            break;
     }
     city_data.festival.planned.size = FESTIVAL_NONE;
     city_data.festival.planned.months_to_go = 0;
