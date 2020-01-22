5d45e0c78686f1d8143d087f844bab17144cfad9
crudelios <crudelios@gmail.com>
2020-01-22 02:37:48 +0000

    Find the actual culprit of the height differences and fix it

--- src/graphics/rich_text.c
+++ src/graphics/rich_text.c
@@ -245,7 +245,7 @@ static void draw_line(const uint8_t *str, int x, int y, color_t color, int measu
                 }
                 const image *img = image_letter(letter_id);
                 if (!measure_only) {
-                    int height = def->image_y_offset(*str, img->height, 11);
+                    int height = def->image_y_offset(*str, img->height, def->line_height);
                     image_draw_letter(def->font, letter_id, x, y - height, color);
                 }
                 x += img->width;
