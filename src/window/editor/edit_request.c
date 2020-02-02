7e4db6a50e152415d9c3721c1cc639232f7751aa
Bianca van Schaik <bvschaik@gmail.com>
2020-02-02 22:18:34 +0100

    Enable money requests in the editor

--- src/window/editor/edit_request.c
+++ src/window/editor/edit_request.c
@@ -118,7 +118,7 @@ static void set_resource(int value)
 }
 static void button_resource(int param1, int param2)
 {
-    window_select_list_show(screen_dialog_offset_x() + 210, screen_dialog_offset_y() + 40, 23, 16, set_resource);
+    window_select_list_show(screen_dialog_offset_x() + 210, screen_dialog_offset_y() + 40, 23, 17, set_resource);
 }
 
 static void set_deadline_years(int value)
