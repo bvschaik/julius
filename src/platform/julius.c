aa0c5a86de825c6c91a82639b910dd68cfcd66d1
Bianca van Schaik <bvschaik@gmail.com>
2020-04-02 20:09:57 +0200

    Add runtime version checks for SDL

--- src/platform/julius.c
+++ src/platform/julius.c
@@ -11,6 +11,7 @@
 #include "platform/cursor.h"
 #include "platform/file_manager.h"
 #include "platform/keyboard_input.h"
+#include "platform/platform.h"
 #include "platform/prefs.h"
 #include "platform/screen.h"
 #include "platform/touch.h"
@@ -394,6 +395,7 @@ static int pre_init(const char *custom_data_dir)
     }
 
     #if SDL_VERSION_ATLEAST(2, 0, 1)
+    if (platform_sdl_version_at_least(2, 0, 1)) {
         char *base_path = SDL_GetBasePath();
         if (base_path) {
             if (platform_file_manager_set_base_path(base_path)) {
@@ -405,6 +407,7 @@ static int pre_init(const char *custom_data_dir)
             }
             SDL_free(base_path);
         }
+    }
     #endif
 
     #ifdef USE_TINYFILEDIALOGS
--- src/platform/prefs.c
+++ src/platform/prefs.c
@@ -1,5 +1,7 @@
 #include "platform/prefs.h"
 
+#include "platform/platform.h"
+
 #include "SDL.h"
 
 #include <stdlib.h>
@@ -9,6 +11,7 @@
 static FILE *open_pref_file(const char *filename, const char *mode)
 {
     #if SDL_VERSION_ATLEAST(2, 0, 1)
+    if (platform_sdl_version_at_least(2, 0, 1)) {
         char *pref_dir = SDL_GetPrefPath("bvschaik", "julius");
         if (!pref_dir) {
             return NULL;
@@ -26,9 +29,9 @@ static FILE *open_pref_file(const char *filename, const char *mode)
         FILE *fp = fopen(pref_file, mode);
         free(pref_file);
         return fp;
-    #else
-        return NULL;
+    }
     #endif
+    return NULL;
 }
 
 const char* pref_data_dir(void)
