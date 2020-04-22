a5c15c96712adfb6f6bbcf1b56acf5ebc91fd8b2
bbright024 <bbright123@yahoo.com>
2020-04-22 09:28:16 -0700

    Removing unnecessary value, adding defines for magic numbers in padding

--- src/widget/sidebar.c
+++ src/widget/sidebar.c
@@ -44,10 +44,14 @@
 #define SIDEBAR_EXPANDED_WIDTH 162
 #define FILLER_Y_OFFSET 474
 
-#define EXTRA_INFO_HEIGHT_LINE_SPACE 32
+#define EXTRA_INFO_LINE_SPACE 32
 #define EXTRA_INFO_HEIGHT_GAME_SPEED 64
 #define EXTRA_INFO_HEIGHT_UNEMPLOYMENT 112
 #define EXTRA_INFO_HEIGHT_RATINGS 272
+#define EXTRA_INFO_TOP_PADDING 10;
+#define EXTRA_INFO_SPEED_PADDING 26;
+#define EXTRA_INFO_UNEMPLOYMENT_TOP_PADDING 20;
+#define EXTRA_INFO_UNEMPLOYMENT_BOTTOM_PADDING 22;
 
 #define SIDEBAR_SLIDE_STEPS 94
 
@@ -330,52 +334,53 @@ static void draw_extra_info_objective(int x_offset, int y_offset, int text_group
     text_draw_number(obj->target, '(', ")", x_offset + 11 + width, y_offset + 16, font);
 }
 
+
 static void draw_extra_info_panel(int x_offset, int extra_info_height)
 {
     int y_offset = FILLER_Y_OFFSET;
-    int y_current_line = y_offset;
-    // Space between lines, when line is purely text (no buttons)
-    int y_line_space = EXTRA_INFO_HEIGHT_LINE_SPACE;
-    
+
     // Borders of the extra_info panel - extends to bottom of screen
     int panel_blocks = extra_info_height / 16;
     graphics_draw_vertical_line(x_offset, y_offset, y_offset + extra_info_height, COLOR_WHITE);
     graphics_draw_vertical_line(x_offset + SIDEBAR_EXPANDED_WIDTH - 1, y_offset, y_offset + extra_info_height, COLOR_SIDEBAR);
     inner_panel_draw(x_offset + 1, y_offset, SIDEBAR_EXPANDED_WIDTH / 16, panel_blocks);
 
+    int y_current_line = y_offset + EXTRA_INFO_TOP_PADDING;
+
     // Game speed modifier in extra_info panel
-    y_current_line += 10;
     lang_text_draw(45, 2, x_offset + 11, y_current_line, FONT_NORMAL_WHITE);
-    y_current_line += 26;
+    y_current_line += EXTRA_INFO_SPEED_PADDING;
+
     text_draw_percentage(data.extra_info.game_speed, x_offset + 60, y_current_line, FONT_NORMAL_GREEN);
     arrow_buttons_draw(x_offset, y_offset, arrow_buttons_speed, 2);
-    y_current_line += y_line_space; 
+    y_current_line += EXTRA_INFO_LINE_SPACE; 
     
     // Unemployment info in extra_info panel
     if (extra_info_height >= EXTRA_INFO_HEIGHT_UNEMPLOYMENT) {
         lang_text_draw(68, 148, x_offset + 11, y_current_line, FONT_NORMAL_WHITE);
-        y_current_line += 20;
+        y_current_line += EXTRA_INFO_UNEMPLOYMENT_TOP_PADDING;
+
         int width = text_draw_percentage(data.extra_info.unemployment_percentage, x_offset + 11, y_current_line, FONT_NORMAL_GREEN);
         text_draw_number(data.extra_info.unemployment_amount, '(', ")", x_offset + 11 + width, y_current_line, FONT_NORMAL_GREEN);
-        y_current_line += 22;
+        y_current_line += EXTRA_INFO_UNEMPLOYMENT_BOTTOM_PADDING;
     }
 
     // Objective value info on extra_info panel (culture, prosperity, peace, etc)
     if (extra_info_height >= EXTRA_INFO_HEIGHT_RATINGS) {
         draw_extra_info_objective(x_offset, y_current_line, 53, 1, &data.extra_info.culture, 0);
-        y_current_line += y_line_space;
+        y_current_line += EXTRA_INFO_LINE_SPACE;
 
         draw_extra_info_objective(x_offset, y_current_line, 53, 2, &data.extra_info.prosperity, 0);
-        y_current_line += y_line_space;
+        y_current_line += EXTRA_INFO_LINE_SPACE;
 
         draw_extra_info_objective(x_offset, y_current_line, 53, 3, &data.extra_info.peace, 0);
-        y_current_line += y_line_space;
+        y_current_line += EXTRA_INFO_LINE_SPACE;
 
         draw_extra_info_objective(x_offset, y_current_line, 53, 4, &data.extra_info.favor, 0);
-        y_current_line += y_line_space;
+        y_current_line += EXTRA_INFO_LINE_SPACE;
 
         draw_extra_info_objective(x_offset, y_current_line, 4, 6, &data.extra_info.population, 1);
-        y_current_line += y_line_space;
+        y_current_line += EXTRA_INFO_LINE_SPACE;
     }
 }
 
