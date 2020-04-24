51380616cec3ac0451cd5d4a704d84d53069af6a
José Cadete <crudelios@gmail.com>
2020-04-24 12:00:45 +0100

    Compile switch and vita builds without using docker (#418)

--- .ci_scripts/run_cmake.sh
+++ .ci_scripts/run_cmake.sh
@@ -2,10 +2,14 @@
 
 case "$BUILD_TARGET" in
 "vita")
-	docker exec vitasdk /bin/bash -c "mkdir build && cd build && cmake -DCMAKE_BUILD_TYPE=Release -DVITA_BUILD=ON .."
+	export VITASDK=/usr/local/vitasdk
+	export PATH=$VITASDK/bin:$PATH
+	mkdir build && cd build && cmake -DCMAKE_BUILD_TYPE=Release -DVITA_BUILD=ON ..
 	;;
 "switch")
-	docker exec switchdev /bin/bash -c "mkdir build && cd build && cmake -DCMAKE_BUILD_TYPE=Release -DSWITCH_BUILD=ON .."
+	export DEVKITPRO=/opt/devkitpro
+	export PATH=/opt/devkitpro/devkitA64/bin:/opt/devkitpro/tools/bin:$PATH
+	mkdir build && cd build && cmake -DCMAKE_BUILD_TYPE=Release -DSWITCH_BUILD=ON ..
 	;;
 "mac")
 	mkdir build && cd build && cmake -DCMAKE_BUILD_TYPE=Release ..
