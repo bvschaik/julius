781f188452e66a2b0bebe9fa893c53356be90e95
Bianca van Schaik <bvschaik@gmail.com>
2020-05-31 20:40:56 +0200

    Move keyboard handling logic to input_box

--- src/widget/input_box.h
+++ src/widget/input_box.h
@@ -1,6 +1,7 @@
 #ifndef WIDGET_INPUT_BOX_H
 #define WIDGET_INPUT_BOX_H
 
+#include "graphics/font.h"
 #include "input/mouse.h"
 
 #define INPUT_BOX_BLOCK_SIZE 16
@@ -10,7 +11,18 @@ typedef struct {
     int y;
     int width_blocks;
     int height_blocks;
+    font_t font;
+    // private vars
+    uint8_t *text;
 } input_box;
+
+void input_box_start(input_box *box, uint8_t *text, int length, int allow_punctuation);
+void input_box_pause(input_box *box);
+void input_box_resume(input_box *box);
+void input_box_stop(input_box *box);
+
+void input_box_refresh_text(input_box *box);
+int input_box_is_accepted(input_box *box);
 
 int input_box_handle_mouse(const mouse *m, const input_box *box);
 void input_box_draw(const input_box *box);
