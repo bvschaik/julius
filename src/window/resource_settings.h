acd7af94bae251f764c5095efc2e79a2aa8982c7
Chris Walsh <attrition.h@gmail.com>
2020-07-07 04:36:08 -0400

    cleanup

--- src/window/resource_settings.h
+++ src/window/resource_settings.h
@@ -3,15 +3,6 @@
 
 #include "game/resource.h"
 
-/**
-    Display a dialog to modify a resources trading status
-
-    @param resource which resource to manipulate
-    @param override_draw_background_fn caller may provide their own background drawing function.
-           this can be necessary as certain windows (like Help) can be larger than the dialog.
-           if override_draw_background_fn is NULL, the standard trade advisor screen background
-           will be shown instead
-*/
 void window_resource_settings_show(resource_type resource);
 
 #endif // WINDOW_RESOURCE_SETTINGS_H
