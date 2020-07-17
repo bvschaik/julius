27820649d44016690638396f2ed97b16f61cf21e
crudelios <crudelios@gmail.com>
2020-07-17 15:15:19 +0100

    Update gradle version, fix merge conflicts

--- src/platform/android/android.h
+++ src/platform/android/android.h
@@ -3,6 +3,8 @@
 
 #ifdef __ANDROID__
 
+#include <stdint.h>
+
 void android_toast_message(const char *messge);
 const char *android_show_c3_path_dialog(void);
 float android_get_screen_scale(void);
@@ -11,6 +13,10 @@ int android_set_base_path(const char *path);
 int android_get_directory_contents(const char *dir, int type, const char *extension, int (*callback)(const char *));
 int android_remove_file(const char *filename);
 int android_is_mouse_in_use(void);
+
+#define PLATFORM_USE_VIRTUAL_KEYBOARD
+void platform_show_virtual_keyboard(const uint8_t *text, int max_length);
+void platform_hide_virtual_keyboard(void);
 
 #endif // __ANDROID__
 #endif // PLATFORM_ANDROID_H
