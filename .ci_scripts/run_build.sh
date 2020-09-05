c5bdb74371135c25c00c57fe12c8debd70166bf4
Bianca van Schaik <bvschaik@gmail.com>
2020-09-05 14:23:59 +0200

    Build release version of Android

--- .ci_scripts/run_build.sh
+++ .ci_scripts/run_build.sh
@@ -23,8 +23,8 @@ case "$BUILD_TARGET" in
 	zip julius.zip julius
 	;;
 "android")
-	cd android && TERM=dumb ./gradlew assembleDebug && \
-	cd .. && cp android/distribution/android/julius/outputs/apk/debug/julius-debug.apk build/julius.apk
+	cd android && TERM=dumb ./gradlew assembleRelease && \
+	cd .. && cp android/distribution/android/julius/outputs/apk/release/julius-release.apk build/julius.apk
 	;;
 *)
 	cd build && make && make test
