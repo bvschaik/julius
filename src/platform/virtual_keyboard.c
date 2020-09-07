55893634bb41acca26c23dbfa384a5357ce0d31d
crudelios <crudelios@gmail.com>
2020-09-07 09:02:16 +0100

    Apply requested changes

--- src/platform/virtual_keyboard.c
+++ src/platform/virtual_keyboard.c
@@ -1,5 +1,6 @@
 #include "game/system.h"
 
+// The following includes are all required as they define PLATFORM_USE_VIRTUAL_KEYBOARD
 #include "android/android.h"
 #include "switch/switch.h"
 #include "vita/vita.h"
