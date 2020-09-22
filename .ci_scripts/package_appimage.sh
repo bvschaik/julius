29bbe5102247536ee812bf7276450527922d3713
Bianca van Schaik <bvschaik@gmail.com>
2020-09-22 13:25:54 +0200

    Test new version of linuxdeploy for AppImage

--- .ci_scripts/package_appimage.sh
+++ .ci_scripts/package_appimage.sh
@@ -6,7 +6,7 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib/
 echo "Available libraries on this system ==>\n$(ldconfig -p)"
 
 #wget https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage
-wget https://artifacts.assassinate-you.net/artifactory/list/linuxdeploy/travis-478/linuxdeploy-x86_64.AppImage
+wget https://artifacts.assassinate-you.net/artifactory/list/linuxdeploy/travis-482/linuxdeploy-x86_64.AppImage
 chmod +x linuxdeploy*.AppImage
 
 ./linuxdeploy-x86_64.AppImage --appdir AppDir --output appimage
