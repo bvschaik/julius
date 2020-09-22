7e1791ac69e20b13a9b0586d5120a2dcf70afa1f
Bianca van Schaik <bvschaik@gmail.com>
2020-09-22 14:07:51 +0200

    Use release build of linuxdeploy for AppImage
    Dependency detection issue has been fixed upstream
    Fixes #515

--- .ci_scripts/package_appimage.sh
+++ .ci_scripts/package_appimage.sh
@@ -5,8 +5,7 @@ cd build
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib/
 echo "Available libraries on this system ==>\n$(ldconfig -p)"
 
-#wget https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage
-wget https://artifacts.assassinate-you.net/artifactory/list/linuxdeploy/travis-482/linuxdeploy-x86_64.AppImage
+wget https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage
 chmod +x linuxdeploy*.AppImage
 
 ./linuxdeploy-x86_64.AppImage --appdir AppDir --output appimage
