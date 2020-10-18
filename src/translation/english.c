c388dfed5accc3b5f7106ed5fe69a5072045cfc5
Keriew <59965174+Keriew@users.noreply.github.com>
2020-10-18 19:32:34 +0200

    Add comment to the new phrase

--- src/translation/english.c
+++ src/translation/english.c
@@ -308,7 +308,8 @@ static translation_string all_strings[] = {
     {TR_BUILDING_SMALL_TEMPLE_MARS_NAME, "Temple of Mars"},
     {TR_BUILDING_SMALL_TEMPLE_VENUS_NAME, "Temple of Venus"},
     {TR_FIGURE_TYPE_PRIEST_BUYER, "Priest" },
-    {TR_PHRASE_FIGURE_MISSIONARY_EXACT_4, "\"I'll do what I can to calm these barbarians down. I'm sure they'll stop attacking the city when I've had a word with them.\"" },
+    // Transcription of mission_exact4.wav
+    {TR_PHRASE_FIGURE_MISSIONARY_EXACT_4, "\"I'll do what I can to calm these barbarians down. I'm sure they'll stop attacking the city when I've had a word with them.\"" }, 
         
 };
 
