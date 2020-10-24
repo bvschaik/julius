59fa4e17533ae5d0e2283c8ef6fe2e9428edcb61
Bianca van Schaik <bvschaik@gmail.com>
2020-10-24 21:43:22 +0200

    Clean up unused files

--- coverage.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-lcov --rc lcov_branch_coverage=1 --capture --directory . --output-file build/coverage.info
-genhtml build/coverage.info --branch-coverage --output-directory build/coverage
--- runtests.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-cd build && find . -name '*.gcda' -exec rm '{}' \; && make && make test && cd .. && ./coverage.sh 
