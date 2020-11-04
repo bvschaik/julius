07ed2fb9277ab5b6096ab90def7581aaf52f6d01
William Hardy Gest <whgest@gmail.com>
2020-11-04 17:10:55 -0600

    Fix build

--- src/widget/top_menu_editor.h
+++ src/widget/top_menu_editor.h
@@ -4,6 +4,7 @@
 #include "input/hotkey.h"
 #include "input/mouse.h"
 
+void menu_file_new_map(int param);
 void widget_top_menu_editor_draw(void);
 int widget_top_menu_editor_handle_input(const mouse *m, const hotkeys *h);
 
