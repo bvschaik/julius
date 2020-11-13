eec1d2ace33b6d84a5ac6feb7c8a937227cf017a
Keriew <59965174+Keriew@users.noreply.github.com>
2020-11-13 17:16:02 +0100

    Fix the check if mods are installed

--- src/game/game.c
+++ src/game/game.c
@@ -110,7 +110,7 @@ int game_init(void)
     load_custom_messages();
     sound_system_init();
     game_state_init();
-    int missing_mods = !mods_get_group_id("Areldir", "Roadblocks"); // If can't find roadblocks mod, mods not installed properly
+    int missing_mods = !mods_get_image_id(mods_get_group_id("Areldir", "Roadblocks"),"roadblock"); // If can't find roadblocks mod, mods not installed properly
     window_logo_show(missing_fonts ? MESSAGE_MISSING_FONTS : (is_unpatched() ? MESSAGE_MISSING_PATCH : (missing_mods ? MESSAGE_MISSING_MODS : MESSAGE_NONE)));
     return 1;
 }
