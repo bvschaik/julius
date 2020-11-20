12ef6bd893bebaec3bae35893764ef4943f3954b
Bianca van Schaik <bvschaik@gmail.com>
2020-11-20 19:24:15 +0100

    Remove Travis

--- .ci_scripts/bintray_link.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-REPO=$(grep '"repo":' bintray.json | head -n1 | grep -o -E '[^":, ]+' | tail -n1)
-RELEASE=$(grep '"name":' bintray.json | head -n1 | grep -o -E '[^":, ]+' | tail -n1)
-VERSION=$(grep '"name":' bintray.json | tail -n1 | grep -o -E '[^":, ]+' | tail -n1)
-
-echo "Bintray link: https://bintray.com/bvschaik/$REPO/$RELEASE/$VERSION#files"
