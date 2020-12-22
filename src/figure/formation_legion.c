e061e632e7aeb2e2c3342190eacd4dcd5e95e748
crudelios <crudelios@gmail.com>
2020-12-22 17:21:11 +0000

    Fix being able to "move" a fort by deleting it and rebuilding it while paused
    This created more bugs than it solved
    
    A "move fort" button would be nice though

--- src/figure/formation_legion.c
+++ src/figure/formation_legion.c
@@ -37,6 +37,9 @@ void formation_legion_delete_for_fort(building *fort)
             if (m->standard_figure_id) {
                 figure_delete(figure_get(m->standard_figure_id));
             }
+            for (int i = 0; i < m->num_figures; ++i) {
+                figure_get(m->figures[i])->action_state = FIGURE_ACTION_149_CORPSE;
+            }
             formation_clear(fort->formation_id);
             formation_calculate_legion_totals();
         }
