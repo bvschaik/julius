eacf84a949f8a6c8db8b58571d89e865c32f2828
Keriew <tomidd@wp.pl>
2021-01-12 12:09:14 +0100

    Switch nonwindows deployment to github actions

--- .ci_scripts/run_build.sh
+++ .ci_scripts/run_build.sh
@@ -16,7 +16,7 @@ case "$BUILD_TARGET" in
 	cd build && make && make install && \
 	echo "Creating disk image" && \
 	hdiutil create -volname Augustus -srcfolder augustus.app -ov -format UDZO augustus.dmg
-	if [[ ! -z "$TRAVIS_TAG" ]]
+	if [[ "$GITHUB_REF" =~ ^refs/tags/v ]]
 	then
 		zip -r augustus.zip augustus.dmg mods maps augustus_manual.pdf
 	else
@@ -31,7 +31,7 @@ case "$BUILD_TARGET" in
 	make DESTDIR=AppDir install && \
 	cd .. && \
 	./.ci_scripts/package_appimage.sh
-	if [[ ! -z "$TRAVIS_TAG" ]]
+	if [[ "$GITHUB_REF" =~ ^refs/tags/v ]]	
 	then
 		zip -r augustus.zip augustus.AppImage mods maps augustus_manual.pdf
 	else
@@ -43,34 +43,11 @@ case "$BUILD_TARGET" in
 	cp -r res/maps ./build	
 	cp -r res/augustus_manual.pdf ./build	
 	cd build && make
-	if [[ ! -z "$TRAVIS_TAG" ]]
+	if [[ "$GITHUB_REF" =~ ^refs/tags/v ]]
 	then
 		zip -r augustus.zip augustus mods maps augustus_manual.pdf
 	else
 		zip -r augustus.zip augustus
 	fi
-	;;
-"android")
-	cd android
-	if [ ! -f julius.keystore ]
-	then
-		COMMAND=assembleDebug
-	elif [ "$TRAVIS_BRANCH" == "master" ]
-	then
-		# Use last commit message for release notes
-		mkdir -p julius/src/main/play/release-notes/en-US
-		git log -1 --pretty=%B > julius/src/main/play/release-notes/en-US/internal.txt
-		COMMAND=publishRelease
-	else
-		COMMAND=assembleRelease
-	fi
-	TERM=dumb ./gradlew $COMMAND
-	if [ -f julius/build/outputs/apk/release/julius-release.apk ]
-	then
-		cp julius/build/outputs/apk/release/julius-release.apk ../build/julius.apk
-	fi
-	;;
-*)
-	cd build && make 
 	;;
 esac
