9e52ea26a3275fc771278f48dce0c34c93651d87
Keriew <tomidd@wp.pl>
2021-02-03 22:09:11 +0100

    Change logfile name

--- src/platform/julius.c
+++ src/platform/julius.c
@@ -90,8 +90,8 @@ static void write_log(void *userdata, int category, SDL_LogPriority priority, co
 static void setup_logging(void)
 {
     // On some platforms (vita, android), not removing the file will not empty it when reopening for writing
-    file_remove("julius-log.txt");
-    log_file = file_open("julius-log.txt", "wt");
+    file_remove("augustus-log.txt");
+    log_file = file_open("augustus-log.txt", "wt");
     SDL_LogSetOutputFunction(write_log, NULL);
 }
 
