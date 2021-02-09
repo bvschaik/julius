86a893baebb0acb1fe7e192b71b086cb6172cae5
José Cadete <crudelios@gmail.com>
2021-02-09 15:24:25 +0000

    Improve icons on Linux:
    - Add Icon to taskbar (missing 32x32 file before)
    - Add icon to window title

---
+++ src/platform/icon.h
@@ -0,0 +1,10 @@
+#ifndef PLATFORM_ICON_H
+#define PLATFORM_ICON_H
+
+/**
+ * Get the window icon's pixels
+ * @return A pointer to the icon's pixels
+ */
+void *platform_icon_get_pixels(void);
+
+#endif // PLATFORM_ICON_H
