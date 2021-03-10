130f5e19c93363fb2316df51695aa4d9d652f030
José Cadete <crudelios@gmail.com>
2021-03-10 23:34:53 +0000

    Upload assets when they change

--- .ci_scripts/build_upload.ps1
+++ .ci_scripts/build_upload.ps1
@@ -46,15 +46,30 @@ if ("${env:COMPILER}" -eq "msvc") {
     throw "Unknown compiler: ${env:COMPILER}"
 }
 
-CopyFile res\maps .
-CopyFile res\augustus_manual.pdf .
-
 $deploy_file = "augustus-$version-$suffix.zip"
+$assets_file = "assets-$version-$repo.zip"
+
+$upload_assets = $false
+
+7z a "$assets_file" assets
+$current_assets_hash = (Get-FileHash "$assets_file").Hash
 
 if ($repo -eq "release") {
+    CopyFile res\maps .
+    CopyFile res\augustus_manual.pdf .
     7z a "deploy\$deploy_file" augustus.exe SDL2.dll SDL2_mixer.dll libmpg123-0.dll assets maps augustus_manual.pdf
 } else {
     7z a "deploy\$deploy_file" augustus.exe SDL2.dll SDL2_mixer.dll libmpg123-0.dll
+
+    if (Test-Path deps\assets_hash.txt) {
+        $previous_assets_hash = Get-Content -TotalCount 1 deps\assets_hash.txt
+
+        if($current_assets_hash -ne $previous_assets_hash) {
+            $upload_assets = $true
+        }
+    } else {
+        $upload_assets = $true
+    }
 }
 
 if (!$?) {
@@ -81,4 +96,15 @@ if ($suffix -eq "windows") {
     echo "Uploaded. URL: https://augustus.josecadete.net/$repo.html"
 } else {
     echo "Not publishing build $suffix - skipping upload"
+}
+
+# Upload assets if they have changed
+if ($upload_assets) {
+    echo "Assets have changed. Uploading $assets_file to $repo/windows/$version"
+    curl -u "$env:UPLOAD_TOKEN" -T "deploy/$assets_file" "https://augustus.josecadete.net/upload/$repo/assets/$version/${assets_file}"
+    if (!$?) {
+        throw "Unable to upload"
+    }
+    echo "Assets uploaded. URL: https://augustus.josecadete.net/$repo.html"
+    $current_assets_hash | Out-File deps\assets_hash.txt
 }
