28bf2b627fabaaa8fc107669f0f17f17c07a5f0b
José Cadete <crudelios@gmail.com>
2021-05-05 10:04:07 +0100

    Fix crash when sending trrops to a distant battle

--- src/window/advisor/imperial.c
+++ src/window/advisor/imperial.c
@@ -214,39 +214,37 @@ void button_request(int index, int param2)
     if (!status) {
         return;
     }
-    int resource_type = city_get_request_resource(index);
-
     city_military_clear_empire_service_legions();
     switch (status) {
-    case CITY_REQUEST_STATUS_NO_LEGIONS_AVAILABLE:
-        window_popup_dialog_show(POPUP_DIALOG_NO_LEGIONS_AVAILABLE, confirm_nothing, 0);
-        break;
-    case CITY_REQUEST_STATUS_NO_LEGIONS_SELECTED:
-        window_popup_dialog_show(POPUP_DIALOG_NO_LEGIONS_SELECTED, confirm_nothing, 0);
-        break;
-    case CITY_REQUEST_STATUS_CONFIRM_SEND_LEGIONS:
-        window_popup_dialog_show(POPUP_DIALOG_SEND_TROOPS, confirm_send_troops, 2);
-        break;
-    case CITY_REQUEST_STATUS_NOT_ENOUGH_RESOURCES:
-        window_popup_dialog_show(POPUP_DIALOG_NOT_ENOUGH_GOODS, confirm_nothing, 0);
-        break;
-    default:
-        selected_resource = resource_type;
-        selected_request_id = (status - CITY_REQUEST_STATUS_MAX) & ~CITY_REQUEST_STATUS_RESOURCES_FROM_GRANARY;
-        if (status & CITY_REQUEST_STATUS_RESOURCES_FROM_GRANARY) {
-            window_popup_dialog_show_confirmation(
-                translation_for(TR_ADVISOR_DISPATCHING_FOOD_FROM_GRANARIES_TITLE),
-                translation_for(TR_ADVISOR_DISPATCHING_FOOD_FROM_GRANARIES_TEXT),
-                city_resource_is_stockpiled(resource_type) ? translation_for(TR_ADVISOR_KEEP_STOCKPILING) : 0,
-                confirm_send_goods);
-        } else {
-            window_popup_dialog_show_confirmation(
-                lang_get_string(5, POPUP_DIALOG_SEND_GOODS),
-                lang_get_string(5, POPUP_DIALOG_SEND_GOODS + 1),
-                city_resource_is_stockpiled(resource_type) ? translation_for(TR_ADVISOR_KEEP_STOCKPILING) : 0,
-                confirm_send_goods);
-        }
-        break;
+        case CITY_REQUEST_STATUS_NO_LEGIONS_AVAILABLE:
+            window_popup_dialog_show(POPUP_DIALOG_NO_LEGIONS_AVAILABLE, confirm_nothing, 0);
+            break;
+        case CITY_REQUEST_STATUS_NO_LEGIONS_SELECTED:
+            window_popup_dialog_show(POPUP_DIALOG_NO_LEGIONS_SELECTED, confirm_nothing, 0);
+            break;
+        case CITY_REQUEST_STATUS_CONFIRM_SEND_LEGIONS:
+            window_popup_dialog_show(POPUP_DIALOG_SEND_TROOPS, confirm_send_troops, 2);
+            break;
+        case CITY_REQUEST_STATUS_NOT_ENOUGH_RESOURCES:
+            window_popup_dialog_show(POPUP_DIALOG_NOT_ENOUGH_GOODS, confirm_nothing, 0);
+            break;
+        default:
+            selected_resource = city_get_request_resource(index);
+            selected_request_id = (status - CITY_REQUEST_STATUS_MAX) & ~CITY_REQUEST_STATUS_RESOURCES_FROM_GRANARY;
+            if (status & CITY_REQUEST_STATUS_RESOURCES_FROM_GRANARY) {
+                window_popup_dialog_show_confirmation(
+                    translation_for(TR_ADVISOR_DISPATCHING_FOOD_FROM_GRANARIES_TITLE),
+                    translation_for(TR_ADVISOR_DISPATCHING_FOOD_FROM_GRANARIES_TEXT),
+                    city_resource_is_stockpiled(selected_resource) ? translation_for(TR_ADVISOR_KEEP_STOCKPILING) : 0,
+                    confirm_send_goods);
+            } else {
+                window_popup_dialog_show_confirmation(
+                    lang_get_string(5, POPUP_DIALOG_SEND_GOODS),
+                    lang_get_string(5, POPUP_DIALOG_SEND_GOODS + 1),
+                    city_resource_is_stockpiled(selected_resource) ? translation_for(TR_ADVISOR_KEEP_STOCKPILING) : 0,
+                    confirm_send_goods);
+            }
+            break;
     }
 }
 
