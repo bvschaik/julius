a5925c52fc245dce9ac403606177aac1e217e16d
Keriew <tomidd@wp.pl>
2021-05-13 00:26:16 +0200

    Make Caravanserai immune to fire and collapse

--- src/building/properties.c
+++ src/building/properties.c
@@ -62,7 +62,7 @@ augustus_building_properties_mapping augustus_building_properties[AUGUSTUS_BUILD
     {BUILDING_LARGE_MAUSOLEUM, {3,1,0,0,0}, "Areldir", "Minor_Monuments", "Mausoleum L"},
     {BUILDING_WATCHTOWER, {2,1,0,0,0}, "Areldir", "Watchtowers", "Watchtower C OFF"},
     {BUILDING_LIBRARY, {2,0,0,0,0}, "Tomasz", "Building_Upgrades", "Downgraded_Library"},
-    {BUILDING_CARAVANSERAI, { 4, 0, 0, 0, 0 },  "Areldir", "Econ_Logistics", "Caravanserai N OFF"},
+    {BUILDING_CARAVANSERAI, { 4, 1, 0, 0, 0 },  "Areldir", "Econ_Logistics", "Caravanserai N OFF"},
     {BUILDING_SMALL_STATUE, {1,1,0,0,-12}, "Lizzaran", "Aesthetics_L", "V Small Statue" }
     //    {BUILDING_PALLISADE, {1,0,0,0,0}, "Areldir", "Palisade", "Palisade"},
 };
