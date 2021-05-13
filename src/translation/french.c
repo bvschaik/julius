09f542b4efbb29e8844120cba6059fb29d57272c
Urides <urien.desterres@gmail.com>
2021-05-13 19:03:23 +0000

    Update french.c (#605)
    Fixed a few typos.
    Added 3 missing strings (based on the current english.c file) and translated it.

--- src/translation/french.c
+++ src/translation/french.c
@@ -4,13 +4,13 @@
 static translation_string all_strings[] = {
     {TR_NO_PATCH_TITLE, "Patch 1.0.1.0 manquant"},
     {TR_NO_PATCH_MESSAGE,
-        "Votre installation de Cesar 3 ne contient pas le patch 1.0.1.0. "
+        "Votre installation de Caesar 3 ne contient pas le patch 1.0.1.0. "
         "Vous pouvez le télécharger depuis : \n"
         URL_PATCHES "\n"
         "Procédez à vos propres risques."},
     {TR_MISSING_FONTS_TITLE, "Polices manquantes"},
     {TR_MISSING_FONTS_MESSAGE,
-        "Votre installation de Cesar 3 requiert des polices d'écriture supplémentaires. "
+        "Votre installation de Caesar 3 requiert des polices d'écriture supplémentaires. "
         "Vous pouvez les télécharger pour votre langue depuis : \n"
         URL_PATCHES},
     {TR_NO_EDITOR_TITLE, "Éditeur manquant"},
@@ -37,6 +37,7 @@ static translation_string all_strings[] = {
     {TR_CONFIG_SHOW_INTRO_VIDEO, "Jouer la vidéo d'introduction"},
     {TR_CONFIG_SIDEBAR_INFO, "Informations supplémentaires dans le panneau latéral"},
     {TR_CONFIG_SMOOTH_SCROLLING, "Activer le défilement doux"},
+    {TR_CONFIG_DISABLE_MOUSE_EDGE_SCROLLING, "Désactiver le défilement de la carte sur le bord de la fenêtre"},
     {TR_CONFIG_DISABLE_RIGHT_CLICK_MAP_DRAG, "Désactiver le clic droit pour déplacer la vue de la cité"},
     {TR_CONFIG_VISUAL_FEEDBACK_ON_DELETE, "Améliore le retour visuel durant le dégagement du terrain"},
     {TR_CONFIG_ALLOW_CYCLING_TEMPLES, "Permet de construire tous les temples successivement"},
@@ -55,7 +56,7 @@ static translation_string all_strings[] = {
     {TR_HOTKEY_HEADER_ADVISORS, "Rapports des conseillers"},
     {TR_HOTKEY_HEADER_OVERLAYS, "Cartes de visualisation"},
     {TR_HOTKEY_HEADER_BOOKMARKS, "Points d'intérêt de la cité"},
-    {TR_HOTKEY_HEADER_EDITOR, "Editeur"},
+    {TR_HOTKEY_HEADER_EDITOR, "Éditeur"},
     {TR_HOTKEY_HEADER_BUILD, "Contrôles de construction"},
     {TR_HOTKEY_ARROW_UP, "Haut"},
     {TR_HOTKEY_ARROW_DOWN, "Bas"},
@@ -92,7 +93,7 @@ static translation_string all_strings[] = {
     {TR_HOTKEY_TOGGLE_OVERLAY, "Basculer sur la carte précédente"},
     {TR_HOTKEY_SHOW_OVERLAY_WATER, "Eau"},
     {TR_HOTKEY_SHOW_OVERLAY_FIRE, "Incendies"},
-    {TR_HOTKEY_SHOW_OVERLAY_DAMAGE, "Dégats"},
+    {TR_HOTKEY_SHOW_OVERLAY_DAMAGE, "Dégâts"},
     {TR_HOTKEY_SHOW_OVERLAY_CRIME, "Criminalité"},
     {TR_HOTKEY_SHOW_OVERLAY_PROBLEMS, "Problèmes"},
     {TR_HOTKEY_GO_TO_BOOKMARK_1, "Aller au point d'intérêt 1"},
@@ -104,7 +105,9 @@ static translation_string all_strings[] = {
     {TR_HOTKEY_SET_BOOKMARK_3, "Définir comme point d'intérêt 3"},
     {TR_HOTKEY_SET_BOOKMARK_4, "Définir comme point d'intérêt 4"},
     {TR_HOTKEY_EDITOR_TOGGLE_BATTLE_INFO, "Afficher les infos de bataille"},
-    {TR_HOTKEY_EDIT_TITLE, "Appuyez sur une touche"}
+    {TR_HOTKEY_EDIT_TITLE, "Appuyez sur une touche"},
+    {TR_HOTKEY_DUPLICATE_TITLE, "Touche de raccourci déjà utilisée"},
+    {TR_HOTKEY_DUPLICATE_MESSAGE, "Cette combinaison de touches est déjà assignée à l'action suivante :"}
 };
 
 void translation_french(const translation_string **strings, int *num_strings)
