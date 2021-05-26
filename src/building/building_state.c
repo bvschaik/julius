34eb42cf29ae6f2694fd7937f68fb013655b66d9
Keriew <tomidd@wp.pl>
2021-05-26 16:19:14 +0200

    Fix compatibility fixes affecting new saves

--- src/building/building_state.c
+++ src/building/building_state.c
@@ -389,28 +389,31 @@ void building_state_load_from_buffer(buffer *buf, building *b, int building_buf_
     b->sentiment.house_happiness = buffer_read_i8(buf); // which union field we use does not matter
     b->show_on_problem_overlay = buffer_read_u8(buf);
 
-    // Backwards compatibility fixes for culture update
-    if (building_monument_is_monument(b) && b->subtype.house_level && b->type != BUILDING_HIPPODROME && b->type <= BUILDING_LIGHTHOUSE) {
-        b->data.monument.phase = b->subtype.house_level;
-    }
-
-    if ((b->type == BUILDING_HIPPODROME || b->type == BUILDING_COLOSSEUM) && !b->data.monument.phase) {
-        b->data.monument.phase = -1;
-    }
-
-    if (((b->type >= BUILDING_LARGE_TEMPLE_CERES && b->type <= BUILDING_LARGE_TEMPLE_VENUS) || b->type == BUILDING_ORACLE) && !b->data.monument.phase) {
-        b->data.monument.phase = -1;
-    }
 
     // Wharves produce meat
     if (b->type == BUILDING_WHARF) {
         b->output_resource_id = RESOURCE_MEAT;
     }
 
-    // Backwards compatibility fixes for sentiment update
-    if (building_buf_size < BUILDING_STATE_STRIKES &&
-        b->house_population && b->sentiment.house_happiness < 20) {
-        b->sentiment.house_happiness = 30;
+    if (building_buf_size < BUILDING_STATE_STRIKES) {
+        // Backwards compatibility fixes for sentiment update
+        if (b->house_population && b->sentiment.house_happiness < 20) {
+            b->sentiment.house_happiness = 30;
+        }
+
+        // Backwards compatibility fixes for culture update
+        if (building_monument_is_monument(b) && b->subtype.house_level && b->type != BUILDING_HIPPODROME && b->type <= BUILDING_LIGHTHOUSE) {
+            b->data.monument.phase = b->subtype.house_level;
+        }
+
+        if ((b->type == BUILDING_HIPPODROME || b->type == BUILDING_COLOSSEUM) && !b->data.monument.phase) {
+            b->data.monument.phase = -1;
+        }
+
+        if (((b->type >= BUILDING_LARGE_TEMPLE_CERES && b->type <= BUILDING_LARGE_TEMPLE_VENUS) || b->type == BUILDING_ORACLE) && !b->data.monument.phase) {
+            b->data.monument.phase = -1;
+        }
+
     }
 
     // To keep backward savegame compatibility, only fill more recent building struct elements
