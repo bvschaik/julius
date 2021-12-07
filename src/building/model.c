3894d738f708ab82c2f936302e96a63ce1279924
Keriew <tomidd@wp.pl>
2021-12-07 02:14:22 +0100

    Remove workers from buildings that need no workers

--- src/building/model.c
+++ src/building/model.c
@@ -99,6 +99,13 @@ static void override_model_data(void)
     buildings[BUILDING_LARGE_TEMPLE_VENUS].desirability_step = 2;
     buildings[BUILDING_LARGE_TEMPLE_VENUS].desirability_step_size = -2;
     buildings[BUILDING_LARGE_TEMPLE_VENUS].desirability_range = 5;
+
+    buildings[BUILDING_WELL].laborers = 0;
+    buildings[BUILDING_GATEHOUSE].laborers = 0;
+    buildings[BUILDING_FORT_JAVELIN].laborers = 0;
+    buildings[BUILDING_FORT_LEGIONARIES].laborers = 0;
+    buildings[BUILDING_FORT_MOUNTED].laborers = 0;
+    buildings[BUILDING_FORT].laborers = 0;
 }
 
 int model_load(void)
