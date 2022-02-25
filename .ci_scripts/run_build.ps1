d343add3e33147116dc56852c048eccfd77c2aba
José Cadete <crudelios@gmail.com>
2022-02-25 11:17:23 +0000

    Restore support for 32 bit and MSVC compilation

--- .ci_scripts/run_build.ps1
+++ .ci_scripts/run_build.ps1
@@ -1,9 +1,9 @@
 mkdir build
 cd build
 if ("${env:COMPILER}" -eq "msvc") {
-    cmake -G "Visual Studio 16 2019" -A x64 -DSYSTEM_LIBS=OFF ..
+    cmake -G "Visual Studio 17 2022" -A x64 -DSYSTEM_LIBS=OFF ..
 } elseif ("${env:COMPILER}" -eq "mingw-32") {
-    $env:path = "C:\msys64\mingw32\bin;${env:path}"
+    $env:path = "D:\msys64\mingw32\bin;${env:path}"
     cmake -G "MinGW Makefiles" -DCMAKE_BUILD_TYPE=Release -DSYSTEM_LIBS=OFF -D CMAKE_C_COMPILER=i686-w64-mingw32-gcc.exe -D CMAKE_MAKE_PROGRAM=mingw32-make.exe ..
 } elseif ("${env:COMPILER}" -eq "mingw-64") {
     cmake -G "MinGW Makefiles" -DCMAKE_BUILD_TYPE=Release -DSYSTEM_LIBS=OFF -D CMAKE_C_COMPILER=x86_64-w64-mingw32-gcc.exe -D CMAKE_MAKE_PROGRAM=mingw32-make.exe ..
