0e03c5f30aefd25b569d95d31e99ede772862b2e
José Cadete <crudelios@gmail.com>
2022-05-10 19:08:30 +0100

    Update VitaSDK

--- .ci_scripts/setup_environment.sh
+++ .ci_scripts/setup_environment.sh
@@ -5,7 +5,7 @@ case "$BUILD_TARGET" in
 	# Note: Using a tagged version of the container to make sure that it's not updated unexpectedly
 	# You can update the tag by obtaining a recent one from here: https://hub.docker.com/r/gnuton/vitasdk-docker/tags
 	# Make sure that it compiles correctly and runs on a Vita prior to pushing the change
-	docker run -d --name vitasdk --workdir /build/git -v "${PWD}:/build/git" gnuton/vitasdk-docker:20220207 tail -f /dev/null
+	docker run -d --name vitasdk --workdir /build/git -v "${PWD}:/build/git" gnuton/vitasdk-docker:20220510 tail -f /dev/null
 	;;
 "switch")
 	docker run -d --name switchdev --workdir /build/git -v "${PWD}:/build/git" devkitpro/devkita64:20200730 tail -f /dev/null
