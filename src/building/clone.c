0bcd30dc9659aeae0f856be1daba466928e98eb1
Mathieu Mahé <mathieu.mahe@gmail.com>
2022-06-12 13:11:40 +0200

    Enable cloning of ruins (#618)

--- src/building/clone.c
+++ src/building/clone.c
@@ -13,13 +13,12 @@
  * For example, given a fort, return the enumaration value corresponding to
  * the specific type of fort rather than the general value
  *
- * @param building Building to examine
+ * @param building Building to examine (can be null for destroyed building)
+ * @param building_type Type of the building to clone (can be original building type before a fire)
  * @return the building_type value to clone, or BUILDING_NONE if not cloneable
  */
-static building_type get_clone_type_from_building(building *b)
+static building_type get_clone_type_from_building(building *b, building_type clone_type)
 {
-    building_type clone_type = b->type;
-
     if (building_is_house(clone_type)) {
         return BUILDING_HOUSE_VACANT_LOT;
     }
@@ -28,26 +27,35 @@ static building_type get_clone_type_from_building(building *b)
         case BUILDING_RESERVOIR:
             return BUILDING_DRAGGABLE_RESERVOIR;
         case BUILDING_FORT:
-            switch (b->subtype.fort_figure_type) {
-                case FIGURE_FORT_LEGIONARY: return BUILDING_FORT_LEGIONARIES;
-                case FIGURE_FORT_JAVELIN: return BUILDING_FORT_JAVELIN;
-                case FIGURE_FORT_MOUNTED: return BUILDING_FORT_MOUNTED;
+            if (b) {
+                switch (b->subtype.fort_figure_type) {
+                    case FIGURE_FORT_LEGIONARY: return BUILDING_FORT_LEGIONARIES;
+                    case FIGURE_FORT_JAVELIN: return BUILDING_FORT_JAVELIN;
+                    case FIGURE_FORT_MOUNTED: return BUILDING_FORT_MOUNTED;
+                }
             }
             return BUILDING_NONE;
         case BUILDING_NATIVE_CROPS:
         case BUILDING_NATIVE_HUT:
         case BUILDING_NATIVE_MEETING:
-        case BUILDING_BURNING_RUIN:
             return BUILDING_NONE;
-        case BUILDING_GARDEN_WALL_GATE: 
+        case BUILDING_BURNING_RUIN:
+            if (b) {
+                return get_clone_type_from_building(b, map_rubble_building_type(b->grid_offset));
+            } else {
+                return BUILDING_NONE;
+            }
+        case BUILDING_GARDEN_WALL_GATE:
         // Check neighbouring tiles to see if it's a part of garden wall.
         // If it is, return that garden wall type, otherwise return default garden wall type.
         {
-            int grid_offset = b->grid_offset;
-            for (const int *tile_delta = map_grid_adjacent_offsets(b->size); *tile_delta; tile_delta++) {
-                building *neighbour = building_get(map_building_at(grid_offset + *tile_delta));
-                if (neighbour->type == BUILDING_ROOFED_GARDEN_WALL || neighbour->type == BUILDING_GARDEN_WALL) {
-                    return neighbour->type;
+            if (b) {
+                int grid_offset = b->grid_offset;
+                for (const int *tile_delta = map_grid_adjacent_offsets(b->size); *tile_delta; tile_delta++) {
+                    building *neighbour = building_get(map_building_at(grid_offset + *tile_delta));
+                    if (neighbour->type == BUILDING_ROOFED_GARDEN_WALL || neighbour->type == BUILDING_GARDEN_WALL) {
+                        return neighbour->type;
+                    }
                 }
             }
             return BUILDING_GARDEN_WALL;
@@ -77,8 +85,10 @@ building_type building_clone_type_from_grid_offset(int grid_offset)
         int building_id = map_building_at(grid_offset);
         if (building_id) {
             building *b = building_main(building_get(building_id));
-            return get_clone_type_from_building(b);
+            return get_clone_type_from_building(b, b->type);
         }
+    } else if (terrain & TERRAIN_RUBBLE) {
+        return get_clone_type_from_building(0, map_rubble_building_type(grid_offset));
     } else if (terrain & TERRAIN_AQUEDUCT) {
         return BUILDING_AQUEDUCT;
     } else if (terrain & TERRAIN_WALL) {
