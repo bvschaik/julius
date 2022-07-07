c454adf70bfc87ed048b1a1f3be398ad22187e3b
José Cadete <crudelios@gmail.com>
2022-07-07 12:31:49 +0100

    Fix meadow not drawing the tops on the water overlay

--- src/widget/city_overlay_other.c
+++ src/widget/city_overlay_other.c
@@ -381,7 +381,7 @@ static int has_deleted_building(int grid_offset)
 static int terrain_on_water_overlay(void)
 {
     return
-        TERRAIN_TREE | TERRAIN_ROCK | TERRAIN_WATER | TERRAIN_SHRUB |
+        TERRAIN_TREE | TERRAIN_ROCK | TERRAIN_WATER | TERRAIN_SHRUB | TERRAIN_MEADOW |
         TERRAIN_GARDEN | TERRAIN_ROAD | TERRAIN_AQUEDUCT | TERRAIN_ELEVATION |
         TERRAIN_ACCESS_RAMP | TERRAIN_RUBBLE;
 }
