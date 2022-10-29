c006100954311cb65ec5cb71239e7c8d5a6f847c
Andrew Whitworth <Whiteknight@users.noreply.github.com>
2022-10-29 16:13:19 -0400

    Herd formation cleanup (#727)
    * Fix issue with animal direction finding in get_roaming_destination
    
    if(direction) doesn't work if DIR_0_TOP is specified, so we have to change it to if(direction < DIR_8_NONE)
    (and I added in a lower-bounds check in case it ever goes negative, just because)
    target_direction needs to be &7 instead of &6 so we keep that ones bit and can move in diagonal directions
    After we move, set m->herd_direction back to DIR_8_NONE instead of DIR_0_TOP
    
    * Only calculate the number of attacking_animals if the animal type is wolf
    
    Saves a lot of unecessary computation when we're dealing with sheep or zebras
    
    * Cleanups for herd formations
    
    pull some magic numbers up to named constants
    Add a few comments to explain what some functions/blocks are doing
    
    * Fix a small (potential) bug where wolves may be able to spawn even if we configure wolf spawning off
    
    If I'm wrong that this can't happen, we just have an extra check to make sure
    
    * Simplify get_roaming_destination signature by passing the formation
    
    This also allows us to cleanup a conditional to be on a single line

--- src/figure/formation_herd.c
+++ src/figure/formation_herd.c
@@ -16,6 +16,17 @@
 #include "scenario/property.h"
 #include "sound/effect.h"
 
+#define FREE_TILE_SEARCH_RADIUS 4
+#define ANIMAL_MOVE_WAIT_TICKS 401
+#define SHEEP_ROAM_DISTANCE 8
+#define SHEEP_ROAM_DELAY 20
+#define ZEBRA_ROAM_DISTANCE 20
+#define ZEBRA_ROAM_DELAY 4
+#define WOLF_ROAM_DISTANCE 16
+#define WOLF_ROAM_DELAY 6
+#define WOLF_SPAWN_DELAY_TICKS 32
+
+// Look for a free tile, in the neighborhood of (x,y)
 static int get_free_tile(int x, int y, int allow_negative_desirability, int *x_tile, int *y_tile)
 {
     unsigned short disallowed_terrain = ~(TERRAIN_ACCESS_RAMP | TERRAIN_MEADOW);
@@ -23,7 +34,7 @@ static int get_free_tile(int x, int y, int allow_negative_desirability, int *x_t
     int x_found = 0, y_found = 0;
 
     int x_min, y_min, x_max, y_max;
-    map_grid_get_area(x, y, 1, 4, &x_min, &y_min, &x_max, &y_max);
+    map_grid_get_area(x, y, 1, FREE_TILE_SEARCH_RADIUS, &x_min, &y_min, &x_max, &y_max);
 
     for (int yy = y_min; yy <= y_max; yy++) {
         for (int xx = x_min; xx <= x_max; xx++) {
@@ -51,14 +62,20 @@ static int get_free_tile(int x, int y, int allow_negative_desirability, int *x_t
     return tile_found;
 }
 
-static int get_roaming_destination(int formation_id, int allow_negative_desirability,
-                                   int x, int y, int distance, int direction, int *x_tile, int *y_tile)
+// Try to find an open destination point (x_tile,y_tile) in the general direction, at the given distance
+static int get_roaming_destination(formation *m, int distance, int *x_tile, int *y_tile)
 {
-    int target_direction = (formation_id + random_byte()) & 6;
-    if (direction) {
+    int formation_id = m->id;
+    int x = m->x_home;
+    int y = m->y_home;
+    int direction = m->herd_direction;
+    int allow_negative_desirability = m->figure_type == FIGURE_WOLF;
+    int target_direction = (formation_id + random_byte()) & 7;
+    if (direction >= DIR_0_TOP && direction < DIR_8_NONE) {
         target_direction = direction;
         allow_negative_desirability = 1;
     }
+    // Do up to 4 checks to find a place to walk to
     for (int i = 0; i < 4; i++) {
         int x_target, y_target;
         switch (target_direction) {
@@ -106,13 +123,12 @@ static int get_roaming_destination(int formation_id, int allow_negative_desirabi
         } else if (y_target >= map_grid_height() - 1) {
             y_target = map_grid_height() - 2;
         }
+        // If we can find a free tile in this direction, return 1
         if (get_free_tile(x_target, y_target, allow_negative_desirability, x_tile, y_tile)) {
             return 1;
         }
-        target_direction += 2;
-        if (target_direction > 6) {
-            target_direction = 0;
-        }
+        // ...otherwise turn right and try again
+        target_direction = (target_direction + 2) % 8;
     }
     return 0;
 }
@@ -128,7 +144,7 @@ static void move_animals(const formation *m, int attacking_animals)
             f->action_state == FIGURE_ACTION_150_ATTACK) {
             continue;
         }
-        f->wait_ticks = 401;
+        f->wait_ticks = ANIMAL_MOVE_WAIT_TICKS;
         if (attacking_animals) {
             int target_id = figure_combat_get_target_for_wolf(f->x, f->y, 6);
             if (target_id) {
@@ -153,7 +169,7 @@ static int can_spawn_wolf(formation *m)
 {
     if (m->num_figures < m->max_figures && m->figure_type == FIGURE_WOLF) {
         m->herd_wolf_spawn_delay++;
-        if (m->herd_wolf_spawn_delay > 32) {
+        if (m->herd_wolf_spawn_delay > WOLF_SPAWN_DELAY_TICKS) {
             m->herd_wolf_spawn_delay = 0;
             return 1;
         }
@@ -161,9 +177,9 @@ static int can_spawn_wolf(formation *m)
     return 0;
 }
 
-static void update_herd_formation(formation *m)
+static void update_herd_formation(formation *m, int infinite_wolves_spawning)
 {
-    if (can_spawn_wolf(m)) {
+    if (infinite_wolves_spawning && can_spawn_wolf(m)) {
         // spawn new wolf
         if (!map_terrain_is(map_grid_offset(m->x, m->y), TERRAIN_IMPASSABLE_WOLF)) {
             figure *wolf = figure_create(m->figure_type, m->x, m->y, DIR_0_TOP);
@@ -172,42 +188,38 @@ static void update_herd_formation(formation *m)
             wolf->wait_ticks = wolf->id & 0x1f;
         }
     }
-    int attacking_animals = 0;
-    for (int fig = 0; fig < MAX_FORMATION_FIGURES; fig++) {
-        int figure_id = m->figures[fig];
-        if (figure_id > 0 && figure_get(figure_id)->action_state == FIGURE_ACTION_150_ATTACK) {
-            attacking_animals++;
-        }
-    }
-    if (m->missile_attack_timeout) {
-        attacking_animals = 1;
-    }
+
     if (m->figures[0]) {
         figure *f = figure_get(m->figures[0]);
         if (f->state == FIGURE_STATE_ALIVE) {
             formation_set_home(m, f->x, f->y);
         }
     }
+
+    int attacking_animals = 0;
     int roam_distance;
     int roam_delay;
-    int allow_negative_desirability;
     switch (m->figure_type) {
         case FIGURE_SHEEP:
-            roam_distance = 8;
-            roam_delay = 20;
-            allow_negative_desirability = 0;
-            attacking_animals = 0;
+            roam_distance = SHEEP_ROAM_DISTANCE;
+            roam_delay = SHEEP_ROAM_DELAY;
             break;
         case FIGURE_ZEBRA:
-            roam_distance = 20;
-            roam_delay = 4;
-            allow_negative_desirability = 0;
-            attacking_animals = 0;
+            roam_distance = ZEBRA_ROAM_DISTANCE;
+            roam_delay = ZEBRA_ROAM_DELAY;
             break;
         case FIGURE_WOLF:
-            roam_distance = 16;
-            roam_delay = 6;
-            allow_negative_desirability = 1;
+            roam_distance = WOLF_ROAM_DISTANCE;
+            roam_delay = WOLF_ROAM_DELAY;
+            for (int fig = 0; fig < MAX_FORMATION_FIGURES; fig++) {
+                int figure_id = m->figures[fig];
+                if (figure_id > 0 && figure_get(figure_id)->action_state == FIGURE_ACTION_150_ATTACK) {
+                    attacking_animals++;
+                }
+            }
+            if (m->missile_attack_timeout) {
+                attacking_animals = 1;
+            }
             break;
         default:
             return;
@@ -220,15 +232,14 @@ static void update_herd_formation(formation *m)
             move_animals(m, attacking_animals);
         } else {
             int x_tile, y_tile;
-            if (get_roaming_destination(m->id, allow_negative_desirability, m->x_home, m->y_home,
-                    roam_distance, m->herd_direction, &x_tile, &y_tile)) {
-                m->herd_direction = 0;
+            if (get_roaming_destination(m, roam_distance, &x_tile, &y_tile)) {
+                m->herd_direction = DIR_8_NONE;
                 if (formation_enemy_move_formation_to(m, x_tile, y_tile, &x_tile, &y_tile)) {
                     formation_set_destination(m, x_tile, y_tile);
                     if (m->figure_type == FIGURE_WOLF && city_sound_update_march_wolf()) {
                         sound_effect_play(SOUND_EFFECT_WOLF_HOWL);
                     }
-                    move_animals(m, attacking_animals);
+                    move_animals(m, 0);
                 }
             }
         }
@@ -244,7 +255,7 @@ void formation_herd_update(void)
         formation *m = formation_get(i);
         int infinite_wolves_spawning = m->figure_type == FIGURE_WOLF && !config_get(CONFIG_GP_CH_DISABLE_INFINITE_WOLVES_SPAWNING);
         if (m->in_use && m->is_herd && !m->is_legion && (m->num_figures > 0 || infinite_wolves_spawning)) {
-            update_herd_formation(m);
+            update_herd_formation(m, infinite_wolves_spawning);
         }
     }
 }
