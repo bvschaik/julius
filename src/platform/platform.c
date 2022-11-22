7f49a720650f013fc7292dbb0f229a52d3d66da7
José Cadete <crudelios@gmail.com>
2022-11-22 15:04:03 +0000

    Bump SDL version and fix a slowdown with the newest version

--- src/platform/platform.c
+++ src/platform/platform.c
@@ -6,11 +6,14 @@
 
 #include <stdlib.h>
 
+static SDL_version version;
+
 int platform_sdl_version_at_least(int major, int minor, int patch)
 {
-    SDL_version v;
-    SDL_GetVersion(&v);
-    return SDL_VERSIONNUM(v.major, v.minor, v.patch) >= SDL_VERSIONNUM(major, minor, patch);
+    if (version.major == 0) {
+        SDL_GetVersion(&version);
+    }
+    return SDL_VERSIONNUM(version.major, version.minor, version.patch) >= SDL_VERSIONNUM(major, minor, patch);
 }
 
 void exit_with_status(int status)
