4a8986ad7868287c8430680fe757e29699f76f95
José Cadete <crudelios@gmail.com>
2023-01-13 17:13:36 +0000

    Prevent luxury palaces close to Pantheum Roma Aeterna from displaying the step necessary for evolution

--- src/building/house_evolution.c
+++ src/building/house_evolution.c
@@ -730,7 +730,7 @@ void building_house_determine_evolve_text(building *house, int worst_desirabilit
         house->data.house.evolve_text_id = 65;
         return;
     }
-    if (level >= HOUSE_LUXURY_PALACE) { // max level!
+    if (house->subtype.house_level >= HOUSE_LUXURY_PALACE) { // max level!
         house->data.house.evolve_text_id = 60;
         return;
     }
