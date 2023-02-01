e984d70e6c1613a01969b2f6177724d684548829
José Cadete <crudelios@gmail.com>
2023-02-01 23:35:53 +0000

    Fix #812 (hopefully)

--- src/map/orientation.c
+++ src/map/orientation.c
@@ -250,7 +250,8 @@ void map_orientation_update_buildings(void)
 {
     for (int i = 1; i < building_count(); i++) {
         building *b = building_get(i);
-        if (b->state == BUILDING_STATE_UNUSED || b->state == BUILDING_STATE_DELETED_BY_GAME) {
+        if (b->state == BUILDING_STATE_UNUSED || b->state == BUILDING_STATE_DELETED_BY_GAME ||
+            b->state == BUILDING_STATE_RUBBLE) {
             continue;
         }
         switch (b->type) {
