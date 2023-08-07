cab4711c7be88fc1beee58f3c0f08e965bd7b780
José Cadete <crudelios@gmail.com>
2023-08-07 12:48:14 +0100

    Fix text input on android

--- src/platform/augustus.c
+++ src/platform/augustus.c
@@ -276,7 +276,7 @@ static void handle_event(SDL_Event *event)
         case SDL_KEYUP:
             platform_handle_key_up(&event->key);
             break;
-#if defined(__ANDROID__) && SDL_VERSION_ATLEAST(2, 24, 0)
+#if defined(__ANDROID__) && SDL_VERSION_ATLEAST(2, 24, 0) && !SDL_VERSION_ATLEAST(2, 24, 1)
         case SDL_TEXTEDITING:
             platform_handle_editing_text(&event->edit);
             break;
