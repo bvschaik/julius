5f78716dd785cd8e09acdaa0479936122f3149f4
UberWaffe <UberWaffe@gmail.com>
2023-09-26 10:25:33 +0200

    fix: XML export of repeating events (#971)
    Fix the max and repeating count of events incorrectly exporting the value of the min.

--- src/scenario/scenario_events_export_xml.c
+++ src/scenario/scenario_events_export_xml.c
@@ -224,10 +224,10 @@ static int export_event(scenario_event_t *event)
         xml_exporter_add_attribute_int("repeat_months_min", event->repeat_months_min);
     }
     if (event->repeat_months_max > 0) {
-        xml_exporter_add_attribute_int("repeat_months_max", event->repeat_months_min);
+        xml_exporter_add_attribute_int("repeat_months_max", event->repeat_months_max);
     }
     if (event->max_number_of_repeats > 0) {
-        xml_exporter_add_attribute_int("max_number_of_repeats", event->repeat_months_min);
+        xml_exporter_add_attribute_int("max_number_of_repeats", event->max_number_of_repeats);
     }
 
     xml_exporter_new_element("conditions", 1);
