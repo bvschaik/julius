aec354ffdc1571a835759c64ab9b685c85ad402f
Keriew <tomidd@wp.pl>
2024-01-30 13:58:48 +0100

    Fix fort name in build menu

--- src/translation/english.c
+++ src/translation/english.c
@@ -1356,7 +1356,7 @@ static translation_string all_strings[] = {
     {TR_WINDOW_ADVISOR_RELIGION_ALTARS_HEADER, "Altars"},
     {TR_REPLAY_MAP_NOT_FOUND_TITLE, "Unable to replay map"},
     {TR_REPLAY_MAP_NOT_FOUND_MESSAGE, "The original map file for this saved game was not found.\nYou need the following file with either a .map or .mapx extension:"},
-    {TR_BUILDING_FORT_AUXILIA_INFANTRY, "Infantry"},
+    {TR_BUILDING_FORT_AUXILIA_INFANTRY, "Auxiliaries - Infantry"},
 };
 
 void translation_english(const translation_string **strings, int *num_strings)
