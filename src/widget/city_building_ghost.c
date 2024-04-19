75f2d9231bd90722a7e2b2dbd9640e9c51b14d3a
José Cadete <crudelios@gmail.com>
2024-04-19 16:21:07 +0100

    Fix concrete maker building ghost wrong climate type

--- src/widget/city_building_ghost.c
+++ src/widget/city_building_ghost.c
@@ -1227,7 +1227,7 @@ static void draw_concrete_maker(const map_tile *tile, int x, int y)
     } else {
         blocked = is_blocked_for_building(grid_offset, building_size, blocked_tiles);
     }
-    int image_id = assets_get_image_id("Industry", "Concrete_Maker_C_ON");
+    int image_id = get_new_building_image_id(grid_offset, BUILDING_CONCRETE_MAKER);
 
     if (blocked) {
         color = COLOR_MASK_BUILDING_GHOST_RED;
