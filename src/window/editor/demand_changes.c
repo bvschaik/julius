8f5cbb32c61aa49c7ea55521b884c559a31d8bb8
José Cadete <crudelios@gmail.com>
2024-05-31 23:43:00 +0100

    Move price changes and demand changes to list box

--- src/window/editor/demand_changes.c
+++ src/window/editor/demand_changes.c
@@ -4,10 +4,10 @@
 #include "empire/trade_route.h"
 #include "game/resource.h"
 #include "graphics/button.h"
-#include "graphics/generic_button.h"
 #include "graphics/graphics.h"
 #include "graphics/image.h"
 #include "graphics/lang_text.h"
+#include "graphics/list_box.h"
 #include "graphics/panel.h"
 #include "graphics/text.h"
 #include "graphics/window.h"
@@ -19,36 +19,25 @@
 #include "window/editor/edit_demand_change.h"
 #include "window/editor/map.h"
 
-static void button_demand_change(int id, int param2);
+static void select_demand_change(unsigned int id, int param2);
+static void draw_demand_change_button(const list_box_item *item);
 
-static generic_button buttons[] = {
-    {20, 42, 290, 25, button_demand_change, button_none, 0, 0},
-    {20, 72, 290, 25, button_demand_change, button_none, 1, 0},
-    {20, 102, 290, 25, button_demand_change, button_none, 2, 0},
-    {20, 132, 290, 25, button_demand_change, button_none, 3, 0},
-    {20, 162, 290, 25, button_demand_change, button_none, 4, 0},
-    {20, 192, 290, 25, button_demand_change, button_none, 5, 0},
-    {20, 222, 290, 25, button_demand_change, button_none, 6, 0},
-    {20, 252, 290, 25, button_demand_change, button_none, 7, 0},
-    {20, 282, 290, 25, button_demand_change, button_none, 8, 0},
-    {20, 312, 290, 25, button_demand_change, button_none, 9, 0},
-    {320, 42, 290, 25, button_demand_change, button_none, 10, 0},
-    {320, 72, 290, 25, button_demand_change, button_none, 11, 0},
-    {320, 102, 290, 25, button_demand_change, button_none, 12, 0},
-    {320, 132, 290, 25, button_demand_change, button_none, 13, 0},
-    {320, 162, 290, 25, button_demand_change, button_none, 14, 0},
-    {320, 192, 290, 25, button_demand_change, button_none, 15, 0},
-    {320, 222, 290, 25, button_demand_change, button_none, 16, 0},
-    {320, 252, 290, 25, button_demand_change, button_none, 17, 0},
-    {320, 282, 290, 25, button_demand_change, button_none, 18, 0},
-    {320, 312, 290, 25, button_demand_change, button_none, 19, 0},
+static list_box_type demand_change_list = {
+    .x = 20,
+    .y = 40,
+    .width_blocks = 38,
+    .height_blocks = 19,
+    .num_columns = 2,
+    .item_height = 30,
+    .extend_to_hidden_scrollbar = 1,
+    .on_select = select_demand_change,
+    .draw_item = draw_demand_change_button
 };
-
-static unsigned int focus_button_id;
 
 static void draw_background(void)
 {
     window_editor_map_draw_all();
+    list_box_request_refresh(&demand_change_list);
 }
 
 static int calc_current_trade(editor_demand_change *from_change, int idx)
@@ -82,6 +71,31 @@ static int calc_current_trade(editor_demand_change *from_change, int idx)
     return amount;
 }
 
+static void draw_demand_change_button(const list_box_item *item)
+{
+    button_border_draw(item->x + 5, item->y + 3, item->width - 10, item->height - 5, item->is_focused);
+    editor_demand_change demand_change;
+    scenario_editor_demand_change_get(item->index, &demand_change);
+    if (demand_change.year) {
+        text_draw_number(demand_change.year, '+', " ", item->x + 15, item->y + 10, FONT_NORMAL_BLACK, 0);
+        lang_text_draw_year(scenario_property_start_year() + demand_change.year, item->x + 40, item->y + 10,
+            FONT_NORMAL_BLACK);
+        image_draw(resource_get_data(demand_change.resource)->image.editor.icon, item->x + 120, item->y + 7,
+            COLOR_MASK_NONE, SCALE_NONE);
+        int width = lang_text_draw(CUSTOM_TRANSLATION, TR_EDITOR_SHORT_ROUTE_TEXT, item->x + 145, item->y + 10,
+            FONT_NORMAL_BLACK);
+        width += text_draw_number(demand_change.route_id, '@', " ", item->x + 145 + width, item->y + 10,
+            FONT_NORMAL_BLACK, 0);
+        int amount = calc_current_trade(&demand_change, item->index);
+        width += text_draw_number(amount, '@', " ", item->x + 145 + width, item->y + 10, FONT_NORMAL_BLACK, 0);
+        int last_amount = calc_current_trade(&demand_change, item->index - 1);
+        width += text_draw_number(amount - last_amount, '(', ")", item->x + 145 + width, item->y + 10,
+            FONT_NORMAL_BLACK, 0);
+    } else {
+        lang_text_draw_centered(44, 96, item->x + 5, item->y + 10, item->width - 10, FONT_NORMAL_BLACK);
+    }
+}
+
 static void draw_foreground(void)
 {
     graphics_in_dialog();
@@ -89,41 +103,14 @@ static void draw_foreground(void)
     outer_panel_draw(0, 0, 40, 23);
     lang_text_draw(44, 94, 20, 14, FONT_LARGE_BLACK);
     lang_text_draw_centered(13, 3, 0, 342, 640, FONT_NORMAL_BLACK);
-
-    for (unsigned int i = 0; i < MAX_DEMAND_CHANGES; i++) {
-        int x, y;
-        if (i < 10) {
-            x = 20;
-            y = 42 + 30 * i;
-        } else {
-            x = 320;
-            y = 42 + 30 * (i - 10);
-        }
-        button_border_draw(x, y, 290, 25, focus_button_id == i + 1);
-        editor_demand_change demand_change;
-        scenario_editor_demand_change_get(i, &demand_change);
-        if (demand_change.year) {
-            text_draw_number(demand_change.year, '+', " ", x + 10, y + 6, FONT_NORMAL_BLACK, 0);
-            lang_text_draw_year(scenario_property_start_year() + demand_change.year, x + 35, y + 6, FONT_NORMAL_BLACK);
-            image_draw(resource_get_data(demand_change.resource)->image.editor.icon, x + 115, y + 3,
-                COLOR_MASK_NONE, SCALE_NONE);
-            int width = lang_text_draw(CUSTOM_TRANSLATION, TR_EDITOR_SHORT_ROUTE_TEXT, x + 140, y + 6, FONT_NORMAL_BLACK);
-            width += text_draw_number(demand_change.route_id, '@', " ", x + 140 + width, y + 6, FONT_NORMAL_BLACK, 0);
-            int amount = calc_current_trade(&demand_change, i);
-            width += text_draw_number(amount, '@', " ", x + 140 + width, y + 6, FONT_NORMAL_BLACK, 0);
-            int last_amount = calc_current_trade(&demand_change, i - 1);
-            width += text_draw_number(amount - last_amount, '(', ")", x + 140 + width, y + 6, FONT_NORMAL_BLACK, 0);
-        } else {
-            lang_text_draw_centered(44, 96, x, y + 6, 290, FONT_NORMAL_BLACK);
-        }
-    }
+    list_box_draw(&demand_change_list);
 
     graphics_reset_dialog();
 }
 
 static void handle_input(const mouse *m, const hotkeys *h)
 {
-    if (generic_buttons_handle_mouse(mouse_in_dialog(m), 0, 0, buttons, 20, &focus_button_id)) {
+    if (list_box_handle_input(&demand_change_list, mouse_in_dialog(m), 1)) {
         return;
     }
     if (input_go_back_requested(m, h)) {
@@ -131,7 +118,7 @@ static void handle_input(const mouse *m, const hotkeys *h)
     }
 }
 
-static void button_demand_change(int id, int param2)
+static void select_demand_change(unsigned int id, int param2)
 {
     window_editor_edit_demand_change_show(id);
 }
@@ -144,5 +131,6 @@ void window_editor_demand_changes_show(void)
         draw_foreground,
         handle_input
     };
+    list_box_init(&demand_change_list, MAX_DEMAND_CHANGES);
     window_show(&window);
 }
--- src/window/editor/price_changes.c
+++ src/window/editor/price_changes.c
@@ -3,50 +3,58 @@
 #include "core/image_group_editor.h"
 #include "game/resource.h"
 #include "graphics/button.h"
-#include "graphics/generic_button.h"
 #include "graphics/graphics.h"
 #include "graphics/image.h"
 #include "graphics/lang_text.h"
+#include "graphics/list_box.h"
 #include "graphics/panel.h"
 #include "graphics/text.h"
 #include "graphics/window.h"
 #include "input/input.h"
+#include "scenario/data.h"
 #include "scenario/editor.h"
 #include "scenario/property.h"
 #include "window/editor/attributes.h"
 #include "window/editor/edit_price_change.h"
 #include "window/editor/map.h"
 
-static void button_price_change(int id, int param2);
+static void select_price_change(unsigned int id, int param2);
+static void draw_price_change_button(const list_box_item *item);
 
-static generic_button buttons[] = {
-    {20, 42, 290, 25, button_price_change, button_none, 0, 0},
-    {20, 72, 290, 25, button_price_change, button_none, 1, 0},
-    {20, 102, 290, 25, button_price_change, button_none, 2, 0},
-    {20, 132, 290, 25, button_price_change, button_none, 3, 0},
-    {20, 162, 290, 25, button_price_change, button_none, 4, 0},
-    {20, 192, 290, 25, button_price_change, button_none, 5, 0},
-    {20, 222, 290, 25, button_price_change, button_none, 6, 0},
-    {20, 252, 290, 25, button_price_change, button_none, 7, 0},
-    {20, 282, 290, 25, button_price_change, button_none, 8, 0},
-    {20, 312, 290, 25, button_price_change, button_none, 9, 0},
-    {320, 42, 290, 25, button_price_change, button_none, 10, 0},
-    {320, 72, 290, 25, button_price_change, button_none, 11, 0},
-    {320, 102, 290, 25, button_price_change, button_none, 12, 0},
-    {320, 132, 290, 25, button_price_change, button_none, 13, 0},
-    {320, 162, 290, 25, button_price_change, button_none, 14, 0},
-    {320, 192, 290, 25, button_price_change, button_none, 15, 0},
-    {320, 222, 290, 25, button_price_change, button_none, 16, 0},
-    {320, 252, 290, 25, button_price_change, button_none, 17, 0},
-    {320, 282, 290, 25, button_price_change, button_none, 18, 0},
-    {320, 312, 290, 25, button_price_change, button_none, 19, 0},
+static list_box_type price_change_list = {
+    .x = 20,
+    .y = 40,
+    .width_blocks = 38,
+    .height_blocks = 19,
+    .num_columns = 2,
+    .item_height = 30,
+    .extend_to_hidden_scrollbar = 1,
+    .on_select = select_price_change,
+    .draw_item = draw_price_change_button
 };
-
-static unsigned int focus_button_id;
 
 static void draw_background(void)
 {
     window_editor_map_draw_all();
+    list_box_request_refresh(&price_change_list);
+}
+
+static void draw_price_change_button(const list_box_item *item)
+{
+    button_border_draw(item->x + 5, item->y + 3, item->width - 10, item->height - 5, item->is_focused);
+    editor_price_change price_change;
+    scenario_editor_price_change_get(item->index, &price_change);
+    if (price_change.year) {
+        text_draw_number(price_change.year, '+', " ", item->x + 15, item->y + 10, FONT_NORMAL_BLACK, 0);
+        lang_text_draw_year(scenario_property_start_year() + price_change.year, item->x + 70, item->y + 10,
+            FONT_NORMAL_BLACK);
+        image_draw(resource_get_data(price_change.resource)->image.editor.icon, item->x + 145, item->y + 7,
+            COLOR_MASK_NONE, SCALE_NONE);
+        int width = lang_text_draw(44, price_change.is_rise ? 104 : 103, item->x + 175, item->y + 10, FONT_NORMAL_BLACK);
+        text_draw_number(price_change.amount, '@', " ", item->x + 175 + width, item->y + 10, FONT_NORMAL_BLACK, 0);
+    } else {
+        lang_text_draw_centered(44, 102, item->x + 5, item->y + 10, item->width - 10, FONT_NORMAL_BLACK);
+    }
 }
 
 static void draw_foreground(void)
@@ -57,37 +65,14 @@ static void draw_foreground(void)
     lang_text_draw(44, 95, 20, 12, FONT_LARGE_BLACK);
     lang_text_draw_centered(13, 3, 0, 456, 640, FONT_NORMAL_BLACK);
     lang_text_draw_multiline(152, 3, 32, 376, 576, FONT_NORMAL_BLACK);
-
-    for (unsigned int i = 0; i < 20; i++) {
-        int x, y;
-        if (i < 10) {
-            x = 20;
-            y = 42 + 30 * i;
-        } else {
-            x = 320;
-            y = 42 + 30 * (i - 10);
-        }
-        button_border_draw(x, y, 290, 25, focus_button_id == i + 1);
-        editor_price_change price_change;
-        scenario_editor_price_change_get(i, &price_change);
-        if (price_change.year) {
-            text_draw_number(price_change.year, '+', " ", x + 10, y + 6, FONT_NORMAL_BLACK, 0);
-            lang_text_draw_year(scenario_property_start_year() + price_change.year, x + 65, y + 6, FONT_NORMAL_BLACK);
-            image_draw(resource_get_data(price_change.resource)->image.editor.icon, x + 140, y + 3,
-                COLOR_MASK_NONE, SCALE_NONE);
-            int width = lang_text_draw(44, price_change.is_rise ? 104 : 103, x + 170, y + 6, FONT_NORMAL_BLACK);
-            text_draw_number(price_change.amount, '@', " ", x + 170 + width, y + 6, FONT_NORMAL_BLACK, 0);
-        } else {
-            lang_text_draw_centered(44, 102, x, y + 6, 290, FONT_NORMAL_BLACK);
-        }
-    }
+    list_box_draw(&price_change_list);
 
     graphics_reset_dialog();
 }
 
 static void handle_input(const mouse *m, const hotkeys *h)
 {
-    if (generic_buttons_handle_mouse(mouse_in_dialog(m), 0, 0, buttons, 20, &focus_button_id)) {
+    if (list_box_handle_input(&price_change_list, mouse_in_dialog(m), 1)) {
         return;
     }
     if (input_go_back_requested(m, h)) {
@@ -95,7 +80,7 @@ static void handle_input(const mouse *m, const hotkeys *h)
     }
 }
 
-static void button_price_change(int id, int param2)
+static void select_price_change(unsigned int id, int param2)
 {
     window_editor_edit_price_change_show(id);
 }
@@ -108,5 +93,6 @@ void window_editor_price_changes_show(void)
         draw_foreground,
         handle_input
     };
+    list_box_init(&price_change_list, MAX_PRICE_CHANGES);
     window_show(&window);
 }
