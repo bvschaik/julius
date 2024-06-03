63707832b2e1e4ad80968ed1d65652a6519e27bc
Jose Cadete <crudelios@gmail.com>
2024-06-03 08:23:52 +0100

    Use list_box for invasions list

--- src/window/editor/invasions.c
+++ src/window/editor/invasions.c
@@ -4,45 +4,52 @@
 #include "graphics/graphics.h"
 #include "graphics/lang_text.h"
 #include "graphics/panel.h"
+#include "graphics/list_box.h"
 #include "graphics/text.h"
 #include "graphics/window.h"
 #include "input/input.h"
+#include "scenario/data.h"
 #include "scenario/editor.h"
 #include "scenario/property.h"
 #include "window/editor/attributes.h"
 #include "window/editor/edit_invasion.h"
 #include "window/editor/map.h"
 
-static void button_invasion(int id, int param2);
+static void select_invasion(unsigned int id, int param2);
+static void draw_invasion_button(const list_box_item *item);
 
-static generic_button buttons[] = {
-    {20, 42, 290, 25, button_invasion, button_none, 0, 0},
-    {20, 72, 290, 25, button_invasion, button_none, 1, 0},
-    {20, 102, 290, 25, button_invasion, button_none, 2, 0},
-    {20, 132, 290, 25, button_invasion, button_none, 3, 0},
-    {20, 162, 290, 25, button_invasion, button_none, 4, 0},
-    {20, 192, 290, 25, button_invasion, button_none, 5, 0},
-    {20, 222, 290, 25, button_invasion, button_none, 6, 0},
-    {20, 252, 290, 25, button_invasion, button_none, 7, 0},
-    {20, 282, 290, 25, button_invasion, button_none, 8, 0},
-    {20, 312, 290, 25, button_invasion, button_none, 9, 0},
-    {320, 42, 290, 25, button_invasion, button_none, 10, 0},
-    {320, 72, 290, 25, button_invasion, button_none, 11, 0},
-    {320, 102, 290, 25, button_invasion, button_none, 12, 0},
-    {320, 132, 290, 25, button_invasion, button_none, 13, 0},
-    {320, 162, 290, 25, button_invasion, button_none, 14, 0},
-    {320, 192, 290, 25, button_invasion, button_none, 15, 0},
-    {320, 222, 290, 25, button_invasion, button_none, 16, 0},
-    {320, 252, 290, 25, button_invasion, button_none, 17, 0},
-    {320, 282, 290, 25, button_invasion, button_none, 18, 0},
-    {320, 312, 290, 25, button_invasion, button_none, 19, 0},
+static list_box_type invasion_list = {
+    .x = 20,
+    .y = 40,
+    .width_blocks = 38,
+    .height_blocks = 19,
+    .num_columns = 2,
+    .item_height = 30,
+    .extend_to_hidden_scrollbar = 1,
+    .on_select = select_invasion,
+    .draw_item = draw_invasion_button
 };
-
-static unsigned int focus_button_id;
 
 static void draw_background(void)
 {
     window_editor_map_draw_all();
+    list_box_request_refresh(&invasion_list);
+}
+
+static void draw_invasion_button(const list_box_item *item)
+{
+    button_border_draw(item->x + 5, item->y + 3, item->width - 10, item->height - 5, item->is_focused);
+    editor_invasion invasion;
+    scenario_editor_invasion_get(item->index, &invasion);
+    if (invasion.type) {
+        text_draw_number(invasion.year, '+', " ", item->x + 15, item->y + 10, FONT_NORMAL_BLACK, 0);
+        lang_text_draw_year(scenario_property_start_year() + invasion.year, item->x + 70, item->y + 10,
+            FONT_NORMAL_BLACK);
+        int width = text_draw_number(invasion.amount, '@', " ", item->x + 125, item->y + 10, FONT_NORMAL_BLACK, 0);
+        lang_text_draw(34, invasion.type, item->x + 120 + width, item->y + 10, FONT_NORMAL_BLACK);
+    } else {
+        lang_text_draw_centered(44, 23, item->x + 5, item->y + 10, item->width - 10, FONT_NORMAL_BLACK);
+    }
 }
 
 static void draw_foreground(void)
@@ -53,35 +60,14 @@ static void draw_foreground(void)
     lang_text_draw(44, 15, 20, 12, FONT_LARGE_BLACK);
     lang_text_draw_centered(13, 3, 0, 456, 640, FONT_NORMAL_BLACK);
     lang_text_draw_multiline(152, 2, 32, 376, 576, FONT_NORMAL_BLACK);
-
-    for (unsigned int i = 0; i < 20; i++) {
-        int x, y;
-        if (i < 10) {
-            x = 20;
-            y = 42 + 30 * i;
-        } else {
-            x = 320;
-            y = 42 + 30 * (i - 10);
-        }
-        button_border_draw(x, y, 290, 25, focus_button_id == i + 1);
-        editor_invasion invasion;
-        scenario_editor_invasion_get(i, &invasion);
-        if (invasion.type) {
-            text_draw_number(invasion.year, '+', " ", x + 6, y + 6, FONT_NORMAL_BLACK, 0);
-            lang_text_draw_year(scenario_property_start_year() + invasion.year, x + 60, y + 6, FONT_NORMAL_BLACK);
-            int width = text_draw_number(invasion.amount, '@', " ", x + 120, y + 6, FONT_NORMAL_BLACK, 0);
-            lang_text_draw(34, invasion.type, x + 115 + width, y + 6, FONT_NORMAL_BLACK);
-        } else {
-            lang_text_draw_centered(44, 23, x, y + 6, 290, FONT_NORMAL_BLACK);
-        }
-    }
+    list_box_draw(&invasion_list);
 
     graphics_reset_dialog();
 }
 
 static void handle_input(const mouse *m, const hotkeys *h)
 {
-    if (generic_buttons_handle_mouse(mouse_in_dialog(m), 0, 0, buttons, 20, &focus_button_id)) {
+    if (list_box_handle_input(&invasion_list, mouse_in_dialog(m), 1)) {
         return;
     }
     if (input_go_back_requested(m, h)) {
@@ -89,7 +75,7 @@ static void handle_input(const mouse *m, const hotkeys *h)
     }
 }
 
-static void button_invasion(int id, int param2)
+static void select_invasion(unsigned int id, int param2)
 {
     window_editor_edit_invasion_show(id);
 }
@@ -102,5 +88,6 @@ void window_editor_invasions_show(void)
         draw_foreground,
         handle_input
     };
+    list_box_init(&invasion_list, MAX_INVASIONS);
     window_show(&window);
 }
