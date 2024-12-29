41e9c98484e342f834b70b7b28dc51de03c12d90
Alex Montgomery <alex@appwapp.com>
2024-12-28 21:17:44 -0500

    add include guards for ios.h

--- src/platform/ios/ios.h
+++ src/platform/ios/ios.h
@@ -1,6 +1,13 @@
+#ifndef PLATFORM_IOS_H
+#define PLATFORM_IOS_H
+
+#ifdef __IPHONEOS__
 #include <TargetConditionals.h>
 
 const char* ios_show_c3_path_dialog(int again);
 void c3_path_chosen(char *new_path);
 const char* ios_get_base_path(void);
 int ios_set_base_path(const char *path);
+
+#endif // __IPHONEOS__
+#endif // PLATFORM_IOS_H
