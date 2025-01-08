2d1c4dcc53644e3379a2c4ca4bd1d87aa141c883
Alex Montgomery <alex@appwapp.com>
2025-01-07 21:05:58 -0500

    cleanup

--- src/platform/ios/ios.h
+++ src/platform/ios/ios.h
@@ -8,7 +8,6 @@
 const char *ios_show_c3_path_dialog(int again);
 void c3_path_chosen(char *new_path);
 const char *ios_get_base_path(void);
-int ios_set_base_path(const char *path);
 
 #endif // __IPHONEOS__
 #endif // PLATFORM_IOS_H
