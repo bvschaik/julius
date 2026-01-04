4fb6f46e197ef50a24b3e5e2e1a2c5197e1f1287
Carlo Bramini <carlo_bramini@users.sourceforge.net>
2026-01-04 02:38:07 +0100

    Add support for Cygwin in backtrace.c

--- src/core/backtrace.c
+++ src/core/backtrace.c
@@ -4,7 +4,7 @@
 
 #if defined(__GNUC__) && !defined(__MINGW32__) && !defined(__OpenBSD__) && \
     !defined(__vita__) && !defined(__SWITCH__) && !defined(__ANDROID__) && \
-    !defined(__HAIKU__) && !defined(__EMSCRIPTEN__) && \
+    !defined(__HAIKU__) && !defined(__EMSCRIPTEN__) && !defined(__CYGWIN__) && \
     (!defined(__ENVIRONMENT_MAC_OS_X_VERSION_MIN_REQUIRED__) || __ENVIRONMENT_MAC_OS_X_VERSION_MIN_REQUIRED__ >= 1050)
 
 #include <execinfo.h>
